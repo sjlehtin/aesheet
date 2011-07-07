fa8c4e44fbcc6df88febd74cb4d4dfddd4428d40
Sami J. Lehtinen <sjl@iki.fi>
2011-07-07 04:07:57 +0300

    Refactored add and remove code for easier reuse.
    Allow adding and removing of spell effects.

--- aesheet/sheet/forms.py
+++ aesheet/sheet/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import widgets
-from sheet.models import CharacterSkill, Weapon, Sheet
+from sheet.models import CharacterSkill, Sheet, SpellEffect, Weapon
 
 class AddWeapon(forms.Form):
     def __init__(self, *args, **kwargs):
@@ -9,21 +9,39 @@ class AddWeapon(forms.Form):
         wpns = Weapon.objects.all()
         choices = [(wpn.id, unicode(wpn)) for wpn in wpns]
         super(AddWeapon, self).__init__(*args, **kwargs)
-        self.fields['weapon'].choices = choices
+        self.fields['item'].choices = choices
         self.fields['form_id'].initial = form_id
 
     form_id = forms.CharField(max_length=64, widget=widgets.HiddenInput)
     # choices overridden in __init__()
-    weapon = forms.ChoiceField(choices=())
+    item = forms.ChoiceField(choices=())
+
+class AddSpellEffect(forms.Form):
+    def __init__(self, *args, **kwargs):
+        sheet = kwargs.pop('sheet')
+        form_id = kwargs.pop('form_id', "add_spell_effect")
+        items = SpellEffect.objects.all()
+        choices = [(item.id, unicode(item)) for item in items]
+        super(AddSpellEffect, self).__init__(*args, **kwargs)
+        self.fields['item'].choices = choices
+        self.fields['form_id'].initial = form_id
+
+    form_id = forms.CharField(max_length=64, widget=widgets.HiddenInput)
+    # choices overridden in __init__()
+    item = forms.ChoiceField(choices=())
 
 class RemoveGeneric(forms.Form):
     def __init__(self, *args, **kwargs):
         item = kwargs.pop('item', None)
         form_id = kwargs.pop('form_id', "")
+        item_type = kwargs.pop('item_type', "")
         super(RemoveGeneric, self).__init__(*args, **kwargs)
         if item:
             self.fields['item'].initial = item.id
+        if item_type:
+            self.fields['item_type'].initial = item_type
         self.fields['form_id'].initial = form_id
 
     form_id = forms.CharField(max_length=64, widget=widgets.HiddenInput)
+    item_type = forms.CharField(max_length=64, widget=widgets.HiddenInput)
     item = forms.IntegerField(widget=widgets.HiddenInput)
--- aesheet/sheet/views.py
+++ aesheet/sheet/views.py
@@ -3,8 +3,9 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
-from sheet.models import Character, Sheet, Weapon
-from sheet.forms import AddWeapon, RemoveGeneric
+from sheet.models import Character, Sheet, SpellEffect, Weapon
+from sheet.forms import AddSpellEffect, AddWeapon, RemoveGeneric
+from django.core.exceptions import ValidationError
 
 def characters_index(request):
     all_characters = Character.objects.all().order_by('name')
@@ -21,64 +22,85 @@ def sheets_index(request):
     return render_to_response('sheet/sheets_index.html', 
                               { 'all_sheets' : all_sheets })
 
-def sheet_add_weapon(request, sheet_id):
-    sheet = get_object_or_404(Sheet, pk=sheet_id)
-    if request.method == "POST":
-        form = AddWeapon(request.POST, sheet=sheet)
+def process_sheet_change_request(request, sheet):
+    form_id = request.POST.get('form_id')
+    if not form_id:
+        raise ValidationError("No form id")
+    if form_id == "remove":
+        item_type = request.POST.get('item_type')
+        if not item_type:
+            raise ValidationError("No item_type")
+        form = RemoveGeneric(request.POST)
+        if form.is_valid():            
+            item = form.cleaned_data['item']
+            if item_type == "weapon":
+                item = get_object_or_404(Weapon, pk=item)
+                sheet.weapons.remove(item)
+            elif item_type == "spell_effect":
+                item = get_object_or_404(SpellEffect, pk=item)
+                sheet.spell_effects.remove(item)
+            else:
+                raise ValidationError("Invalid item type")
+            sheet.full_clean()
+            sheet.save()
+            return HttpResponseRedirect('/sheets/%s/' % sheet.id)
+        # removal forms are forgotten and not updated on failures.
+
+    elif form_id == "add_weapon":
+        form = AddWeapon(request.POST, sheet=sheet, form_id=form_id)
         if form.is_valid():
-            weapon = form.cleaned_data['weapon']
+            weapon = add_weapon_form.cleaned_data['item']
             weapon = get_object_or_404(Weapon, pk=weapon)
             sheet.weapons.add(weapon)
             sheet.full_clean()
             sheet.save()
             return HttpResponseRedirect('/sheets/%s/' % sheet.id)
-    else:
-        form = AddWeapon(sheet=sheet)
-    return render_to_response('sheet/add_weapon.html',
-                              { 'form' : form,
-                                'char' : sheet },
-                              context_instance=RequestContext(request))
+
+    elif form_id == "add_spell_effect":
+        form = AddSpellEffect(request.POST, sheet=sheet, form_id=form_id)
+        if form.is_valid():
+            spell = form.cleaned_data['item']
+            spell = get_object_or_404(SpellEffect, pk=spell)
+            sheet.spell_effects.add(spell)
+            sheet.full_clean()
+            sheet.save()
+            return HttpResponseRedirect('/sheets/%s/' % sheet.id)
+
 
 def sheet_detail(request, sheet_id):
     sheet = get_object_or_404(Sheet, pk=sheet_id)
 
     add_weapon_form = AddWeapon(sheet=sheet, 
                                 form_id="add_weapon")
+    add_spell_form = AddSpellEffect(sheet=sheet, 
+                                    form_id="add_spell_effect")
 
     if request.method == "POST":
-        if request.POST.get('form_id') == "remove_weapon":
-            remove_weapon_form = RemoveGeneric(request.POST)
-            print "*** fooo %s" % remove_weapon_form
-            print "*** fooo2 %s" % dir(remove_weapon_form)
-            if remove_weapon_form.is_valid():
-                weapon = remove_weapon_form.cleaned_data['item']
-                weapon = get_object_or_404(Weapon, pk=weapon)
-                sheet.weapons.remove(weapon)
-                sheet.full_clean()
-                sheet.save()
-                return HttpResponseRedirect('/sheets/%s/' % sheet.id)
-            # removal forms are forgotten and not updated on failures.
-        else:
-            add_weapon_form = AddWeapon(request.POST, sheet=sheet, 
-                                        form_id="add_weapon")
-            if add_weapon_form.is_valid():
-                weapon = add_weapon_form.cleaned_data['weapon']
-                weapon = get_object_or_404(Weapon, pk=weapon)
-                sheet.weapons.add(weapon)
-                sheet.full_clean()
-                sheet.save()
-                return HttpResponseRedirect('/sheets/%s/' % sheet.id)
-
+        process_sheet_change_request(request, sheet)
+        # XXX more complex forms need to be passed back to
+        # render_to_response, below.
     weapons = []
     if sheet.weapons.exists():
-        weapons = [{ 'weapon' : wpn,
+        weapons = [{ 'item' : wpn,
                      'remove_form' : RemoveGeneric(item=wpn, 
-                                                   form_id="remove_weapon") } 
+                                                   form_id="remove",
+                                                   item_type="weapon") } 
                    for wpn in sheet.weapons.all()]
         
+    spell_effects = []
+    if sheet.spell_effects.exists():
+        spell_effects = [{ 'item' : item,
+                           'remove_form' : RemoveGeneric(
+                    item=item, 
+                    form_id="remove",
+                    item_type="spell_effect") } 
+                   for item in sheet.spell_effects.all()]
+
     return render_to_response('sheet/sheet_detail.html', 
                               { 'char' : sheet,
                                 'weapons' : weapons,
+                                'spell_effects' : spell_effects,
                                 'add_weapon_form' : add_weapon_form,
+                                'add_spell_effect_form' : add_spell_form,
                                 },
                               context_instance=RequestContext(request))
