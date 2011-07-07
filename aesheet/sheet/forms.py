f29621f8956c7ddb358cacea1c602d26be94d1ca
Sami J. Lehtinen <sjl@iki.fi>
2011-07-07 03:08:42 +0300

    Also allow removal of weapons.

--- aesheet/sheet/forms.py
+++ aesheet/sheet/forms.py
@@ -1,13 +1,29 @@
 from django import forms
+from django.forms import widgets
 from sheet.models import CharacterSkill, Weapon, Sheet
 
 class AddWeapon(forms.Form):
     def __init__(self, *args, **kwargs):
         sheet = kwargs.pop('sheet')
+        form_id = kwargs.pop('form_id', "add_weapon")
         wpns = Weapon.objects.all()
         choices = [(wpn.id, unicode(wpn)) for wpn in wpns]
         super(AddWeapon, self).__init__(*args, **kwargs)
         self.fields['weapon'].choices = choices
+        self.fields['form_id'].initial = form_id
 
+    form_id = forms.CharField(max_length=64, widget=widgets.HiddenInput)
     # choices overridden in __init__()
     weapon = forms.ChoiceField(choices=())
+
+class RemoveWeapon(forms.Form):
+    def __init__(self, *args, **kwargs):
+        weapon = kwargs.pop('weapon', None)
+        form_id = kwargs.pop('form_id', "remove_weapon")
+        super(RemoveWeapon, self).__init__(*args, **kwargs)
+        if weapon:
+            self.fields['weapon'].initial = weapon.id
+        self.fields['form_id'].initial = form_id
+
+    form_id = forms.CharField(max_length=64, widget=widgets.HiddenInput)
+    weapon = forms.IntegerField(widget=widgets.HiddenInput)
--- aesheet/sheet/views.py
+++ aesheet/sheet/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from sheet.models import Character, Sheet, Weapon
-from sheet.forms import AddWeapon
+from sheet.forms import AddWeapon, RemoveWeapon
 
 def characters_index(request):
     all_characters = Character.objects.all().order_by('name')
@@ -41,18 +41,43 @@ def sheet_add_weapon(request, sheet_id):
 
 def sheet_detail(request, sheet_id):
     sheet = get_object_or_404(Sheet, pk=sheet_id)
+
+    add_weapon_form = AddWeapon(sheet=sheet, 
+                                form_id="add_weapon")
+
     if request.method == "POST":
-        weapon_form = AddWeapon(request.POST, sheet=sheet)
-        if form.is_valid():
-            weapon = form.cleaned_data['weapon']
-            weapon = get_object_or_404(Weapon, pk=weapon)
-            sheet.weapons.add(weapon)
-            sheet.full_clean()
-            sheet.save()
-            return HttpResponseRedirect('/sheets/%s/' % sheet.id)
-    else:
-        weapon_form = AddWeapon(sheet=sheet)
+        if request.POST.get('form_id') == "remove_weapon":
+            remove_weapon_form = RemoveWeapon(request.POST)
+            print "*** fooo %s" % remove_weapon_form
+            print "*** fooo2 %s" % dir(remove_weapon_form)
+            if remove_weapon_form.is_valid():
+                weapon = remove_weapon_form.cleaned_data['weapon']
+                weapon = get_object_or_404(Weapon, pk=weapon)
+                sheet.weapons.remove(weapon)
+                sheet.full_clean()
+                sheet.save()
+                return HttpResponseRedirect('/sheets/%s/' % sheet.id)
+        else:
+            add_weapon_form = AddWeapon(request.POST, sheet=sheet, 
+                                        form_id="add_weapon")
+            if add_weapon_form.is_valid():
+                weapon = add_weapon_form.cleaned_data['weapon']
+                weapon = get_object_or_404(Weapon, pk=weapon)
+                sheet.weapons.add(weapon)
+                sheet.full_clean()
+                sheet.save()
+                return HttpResponseRedirect('/sheets/%s/' % sheet.id)
+
+    weapons = []
+    if sheet.weapons.exists():
+        weapons = [{ 'weapon' : wpn,
+                     'remove_form' : RemoveWeapon(weapon=wpn, 
+                                                  form_id="remove_weapon") } 
+                   for wpn in sheet.weapons.all()]
+        
     return render_to_response('sheet/sheet_detail.html', 
                               { 'char' : sheet,
-                                'weapon_form' : weapon_form,},
+                                'weapons' : weapons,
+                                'add_weapon_form' : add_weapon_form,
+                                },
                               context_instance=RequestContext(request))
