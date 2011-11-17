d04c16e4a1d1faecec8f8609b7bca3c096a0737c
Sami J. Lehtinen <sjl@iki.fi>
2011-11-17 14:01:53 +0200

    Added ArmorSpecialQuality to admin site.

--- sheet/admin.py
+++ sheet/admin.py
@@ -3,13 +3,14 @@ from sheet.models import *
 from django.contrib import admin
 
 class WeaponTemplateAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ccv', 'draw_initiative', 'roa', 'num_dice', 
-                    'dice', 'extra_damage', 'leth', 'plus_leth', 
+    list_display = ('name', 'ccv', 'draw_initiative', 'roa', 'num_dice',
+                    'dice', 'extra_damage', 'leth', 'plus_leth',
                     'defense_leth', 'type', 'durability', 'dp', 'short_name',
                     'notes', 'is_lance', 'base_skill', 'skill', 'skill2')
 admin.site.register(Armor)
 admin.site.register(ArmorQuality)
 admin.site.register(ArmorTemplate)
+admin.site.register(ArmorSpecialQuality)
 admin.site.register(Character)
 admin.site.register(CharacterEdge)
 admin.site.register(CharacterSkill)
