9278398c942b7ff0228d8fe6135725b17a778064
Sami J. Lehtinen <sjl@iki.fi>
2012-02-16 01:09:48 +0200

    Removed dajax and dajaxice, most likely not needed.

--- settings.py
+++ settings.py
@@ -175,8 +175,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'dajaxice',
-    'dajax',
     'south',
     'sheet'
 )
