627b81212164161a3dd81608bcd79b1e2119de9f
Sami J. Lehtinen <sjl@iki.fi>
2012-02-17 01:17:17 +0200

    Use PositiveIntegerField instead of homegrown validation function.

--- sheet/models.py
+++ sheet/models.py
@@ -7,10 +7,6 @@ from functools import wraps
 
 from django.core.exceptions import ValidationError
 
-def validate_nonnegative(value):
-    if value < 0:
-        raise ValidationError(u'%s is negative' % value)
-
 SIZE_CHOICES = (
     ('F', 'Fine'),
     ('D', 'Diminutive'),
@@ -47,67 +43,43 @@ class Character(models.Model):
     # e.g., GM usage.
     race = models.CharField(max_length=256)
     description = models.TextField(blank=True)
-    age =  models.IntegerField(validators=[validate_nonnegative], default=20)
+    age =  models.PositiveIntegerField(default=20)
     unnatural_aging = models.IntegerField(default=0)
     height = models.IntegerField(default=175)
     weigth = models.IntegerField(default=75)
-    times_wounded  =  models.IntegerField(validators=[validate_nonnegative],
-                                          default=0)
+    times_wounded  =  models.PositiveIntegerField(default=0)
     size = models.CharField(max_length=1, choices=SIZE_CHOICES, default='M')
 
     deity = models.CharField(max_length=256, default="Kord")
-    adventures = models.IntegerField(validators=[validate_nonnegative],
-                                     default=0)
-    gained_sp = models.IntegerField(validators=[validate_nonnegative],
-                                     default=0)
+    adventures = models.PositiveIntegerField(default=0)
+    gained_sp = models.PositiveIntegerField(default=0)
 
-    xp_used_ingame = models.IntegerField(validators=[validate_nonnegative],
-                                         default=0)
-    bought_stamina = models.IntegerField(validators=[validate_nonnegative],
-                                         default=0)
-    bought_mana = models.IntegerField(validators=[validate_nonnegative],
-                                      default=0)
-    edges_bougth = models.IntegerField(validators=[validate_nonnegative],
-                                       default=0)
-    total_xp = models.IntegerField(validators=[validate_nonnegative],
-                                   default=0)
+    xp_used_ingame = models.PositiveIntegerField(default=0)
+    bought_stamina = models.PositiveIntegerField(default=0)
+    bought_mana = models.PositiveIntegerField(default=0)
+    edges_bougth = models.PositiveIntegerField(default=0)
+    total_xp = models.PositiveIntegerField(default=0)
 
     # The abilities the character was rolled with.
-    start_fit = models.IntegerField(validators=[validate_nonnegative],
-                                    default=43)
-    start_ref = models.IntegerField(validators=[validate_nonnegative],
-                                    default=43)
-    start_lrn = models.IntegerField(validators=[validate_nonnegative],
-                                    default=43)
-    start_int = models.IntegerField(validators=[validate_nonnegative],
-                                    default=43)
-    start_psy = models.IntegerField(validators=[validate_nonnegative],
-                                    default=43)
-    start_wil = models.IntegerField(validators=[validate_nonnegative],
-                                    default=43)
-    start_cha = models.IntegerField(validators=[validate_nonnegative],
-                                    default=43)
-    start_pos = models.IntegerField(validators=[validate_nonnegative],
-                                    default=43)
+    start_fit = models.PositiveIntegerField(default=43)
+    start_ref = models.PositiveIntegerField(default=43)
+    start_lrn = models.PositiveIntegerField(default=43)
+    start_int = models.PositiveIntegerField(default=43)
+    start_psy = models.PositiveIntegerField(default=43)
+    start_wil = models.PositiveIntegerField(default=43)
+    start_cha = models.PositiveIntegerField(default=43)
+    start_pos = models.PositiveIntegerField(default=43)
 
     # Current ability scores, i.e., start ability plus increases with
     # XP.
-    cur_fit = models.IntegerField(validators=[validate_nonnegative],
-                                  default=43)
-    cur_ref = models.IntegerField(validators=[validate_nonnegative],
-                                  default=43)
-    cur_lrn = models.IntegerField(validators=[validate_nonnegative],
-                                  default=43)
-    cur_int = models.IntegerField(validators=[validate_nonnegative],
-                                  default=43)
-    cur_psy = models.IntegerField(validators=[validate_nonnegative],
-                                  default=43)
-    cur_wil = models.IntegerField(validators=[validate_nonnegative],
-                                  default=43)
-    cur_cha = models.IntegerField(validators=[validate_nonnegative],
-                                  default=43)
-    cur_pos = models.IntegerField(validators=[validate_nonnegative],
-                                  default=43)
+    cur_fit = models.PositiveIntegerField(default=43)
+    cur_ref = models.PositiveIntegerField(default=43)
+    cur_lrn = models.PositiveIntegerField(default=43)
+    cur_int = models.PositiveIntegerField(default=43)
+    cur_psy = models.PositiveIntegerField(default=43)
+    cur_wil = models.PositiveIntegerField(default=43)
+    cur_cha = models.PositiveIntegerField(default=43)
+    cur_pos = models.PositiveIntegerField(default=43)
 
     # Permanent modifiers to ability scores.
     base_mod_fit = models.IntegerField(default=0)
