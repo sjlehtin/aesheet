f079418d0203c03ba6dab2fba1b4d5d6061f45f6
Sami J. Lehtinen <sjl@iki.fi>
2012-02-17 04:49:29 +0200

    Leverage URL reverse lookups more.

--- sheet/urls.py
+++ sheet/urls.py
@@ -7,9 +7,8 @@ urlpatterns = patterns(
     url(r'^characters/(?P<char_id>\d+)/$', 'character_detail'),
     url(r'^characters/add_char/$', 'edit_character',
         name="add_char"),
-    url(r'^characters/edit_char/$', 'edit_character'),
-    url(r'^characters/edit_char/(?P<char_id>\d+)/$',
-        'edit_character', name="edit_char"),
+
+    url(r'^characters/edit_char/(?P<char_id>\d+)?/?$', 'edit_character'),
 
     url(r'^sheets/edit_sheet/(?P<sheet_id>\d+)?/?$', 'edit_sheet'),
 
