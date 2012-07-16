f8976e7c6bc55dbb9dc800be9c3988920396734b
Sami J. Lehtinen <sjl@iki.fi>
2012-07-16 17:58:43 +0300

    Log sheet messages to console on request.

--- settings.py
+++ settings.py
@@ -230,25 +230,28 @@ DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': INTERCEPT_REDIRECTS
     }
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+if os.getenv('LOG_TO_CONSOLE'):
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'simple': {
+                'format': '%(levelname).1s[%(relativeCreated)d] '
+                '%(filename)s:%(lineno)s: %(message)s'
+            },
         },
+        'handlers': {
+            'console':{
+                'level':'DEBUG',
+                'class':'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+        },
+        'loggers': {
+            'sheet' : {
+                'handlers' : ['console'],
+                'level' : 'DEBUG'
+                }
+
+        }
     }
-}
