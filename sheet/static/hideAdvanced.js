c7e6182f24e135015b6ed051814cf74b2ba54d37
Sami J. Lehtinen <sjl@iki.fi>
2013-12-22 22:36:54 +0200

    Moved larger sections in the sheet to separate files to make the main sheet file more manageable.  Most of the edit controls start out hidden so as not to clutter the page.

--- sheet/static/hideAdvanced.js
+++ sheet/static/hideAdvanced.js
@@ -2,32 +2,47 @@
     $.widget( "sheet.hideAdvanced", {
 	options: {
 	    showAdvancedText: 'Show advanced',
-	    startOpen: false
+	    startOpen: false,
+        style: "button" // plain or button.
 	},
 
 	toggle: function () {
 	    var should_hide = this.element.is(':visible');
 	    var button_text = 'Hide';
 	    if (should_hide) {
-		button_text = this.options.showAdvancedText;
+            button_text = this.options.showAdvancedText;
 	    }
-	    this._button.button('option', 'text', button_text);
+        if (this.options.style === 'button') {
+            this._button.button('option', 'label', button_text);
+        } else {
+            this._button.text(button_text);
+        }
 	    this.element.toggle(!should_hide);
 	},
 
 	_create: function() {
 	    var that = this;
-	    this._button = $('<button input="button" ' +
+        var advanced_text = this.element.data('advanced-text');
+        if (advanced_text) {
+            this.options.showAdvancedText = advanced_text;
+        }
+        this._button = $('<a href="javascript:void(0)"' +
 			     'class="show-hide-advanced">' + 
-			     this.options.showAdvancedText + 
-			     '</button>').button();
-	    this._button.click(function () {
-		that.toggle();
-	    });
+			     this.options.showAdvancedText +
+			     '</a>');
+
+        if (this.options.style === "button") {
+            this._button.button();
+        }
+
+        this._button.click(function () {
+            that.toggle();
+        });
+
 	    this.element.before(this._button);
-	    this.toggle();
+        this.toggle();
 	    if (this.options.startOpen) {
-		this.toggle();
+            this.toggle();
 	    }
 	}
     });
