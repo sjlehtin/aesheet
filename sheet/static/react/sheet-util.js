3a62d41b9bd3edbb02c75632d275764a3d76e014
Sami J. Lehtinen <sjl@iki.fi>
2016-01-05 03:53:51 +0200

    Fixed isFloat.

--- sheet/static/react/sheet-util.js
+++ sheet/static/react/sheet-util.js
@@ -1,39 +1,41 @@
-var isInt = function (value, method) {
+
+var exports = function () {
     "use strict";
-    /* Checking if a number is an integer
-     *
-     *  http://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
-     **/
 
-    if (method === undefined) {
-        method = parseInt;
-    }
-    if (isNaN(value)) {
-        return false;
-    }
-    if ((value|0) === method(value)) {
-        return true;
-    }
-    return false;
-};
+    var isInt = function (value) {
+            /* Checking if a number is an integer
+             *
+             *  http://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
+             **/
 
-var isFloat = function (value) {
-    return isInt(value, parseFloat);
-    "use strict";
-    /* Checking if a number is an integer
-     *
-     *  http://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
-     **/
-    if (isNaN(value)) {
-        return false;
-    }
-    if ((value|0) === parseInt(value)) {
-        return true;
-    }
-    return false;
-};
+            if (isNaN(value)) {
+                return false;
+            }
+            if ((value | 0) === parseInt(value)) {
+                return true;
+            }
+            return false;
+        };
 
-module.exports = {
-    'isInt': isInt,
-    'isFloat': isFloat
-}
+    return {
+        isInt: isInt,
+
+        isFloat: function (value) {
+            if (typeof(value) === "number") {
+                return true;
+            }
+            if (isNaN(value)) {
+                return false;
+            }
+            if (isInt(value)) {
+                return true;
+            }
+            if (/\d\d*(\.\d\d*)?/.test(value)) {
+                return true;
+            }
+            return false;
+        }
+    }
+}();
+
+module.exports = exports;
