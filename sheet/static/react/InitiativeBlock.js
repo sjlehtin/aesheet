b24ede9a7e96d481e5bd53ca358226d75b8cee83
Sami J. Lehtinen <sjl@iki.fi>
2016-01-18 23:05:51 +0200

    Added free-form input field for initiatives.

--- sheet/static/react/InitiativeBlock.js
+++ sheet/static/react/InitiativeBlock.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Panel, Table} from 'react-bootstrap';
+import {Panel, Table, Input} from 'react-bootstrap';
 
 var util = require('sheet-util');
 
@@ -8,7 +8,7 @@ class InitiativeBlock extends React.Component {
 
     constructor(props) {
         super(props);
-
+        this.state = {distance: 30};
     }
 
     initiatives(constant, distances) {
@@ -20,17 +20,35 @@ class InitiativeBlock extends React.Component {
     }
 
     render() {
-        var distances = [30, 20, 10, 5, 2];
+        var distances = [this.state.distance, 20, 10, 5, 2];
         var charging = this.initiatives(20, distances);
         var melee = this.initiatives(30, distances);
         var ranged = this.initiatives(60, distances);
         var getItems = function (initiatives) {
             return initiatives.map((init, ii) => { return <td key={ii}>{init}</td>; });
         }
-        return <Panel style={this.props.style} header={<h4>Initiative penalty for advancing in combat</h4>}>
+        return <Panel style={this.props.style} header={
+          <h4>Initiative penalty for advancing in combat</h4>}>
             <Table style={{fontSize: "inherit"}} fill>
                 <thead>
-                    <tr><th>Distance</th>{distances.map((elem, ii) => { return <th key={ii}>{elem} m</th>})}</tr>
+                    <tr>
+                        <th>Distance</th>
+                        <th>
+                            <input type="text" style={{width: "3em"}}
+                                //style={{fontSize: "inherit"}}
+                                   //style={{fontSize: "inherit", height:
+                                // "2em", width: "3em", paddingLeft: 5, textAlign: "right", }}
+                                   //addonAfter="m"
+                                   value={this.state.distance}
+                                   //ref={(c) => {
+                                   //if (c) {
+                                   //  this._inputNode = c.getInputDOMNode()}}}
+                                ref={(c) => { this._inputNode = c }}
+                                   onChange={(e) => {this.setState({distance: e.target.value})}}
+                        /> m
+                        </th>
+                        {distances.slice(1).map((elem, ii) => {
+                            return <th key={ii}>{elem} m</th>})}</tr>
                 </thead>
                 <tbody>
                 <tr><td>Charge</td>{getItems(charging)}</tr>
