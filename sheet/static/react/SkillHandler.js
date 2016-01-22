a37c2c4c35503bc59644507e5522bd1719d03307
Sami J. Lehtinen <sjl@iki.fi>
2016-01-23 01:26:26 +0200

    Added SkillHandler.
    This will be used as the go-to point for skill calculations in the
    future.

---
+++ sheet/static/react/SkillHandler.js
@@ -0,0 +1,95 @@
+class SkillHandler {
+    constructor(props) {
+        this.props = props;
+        this.state = {
+            characterSkillMap: SkillHandler.getCharacterSkillMap(this.props.characterSkills),
+            skillMap: SkillHandler.getSkillMap(this.props.allSkills)
+        }
+    }
+
+    /* TODO: Moved here from SkillTable. */
+    static getCharacterSkillMap(skillList) {
+        var csMap = {};
+        for (let cs of skillList) {
+            csMap[cs.skill] = cs;
+        }
+        return csMap;
+    }
+
+    static getSkillMap(skillList) {
+        var skillMap = {};
+        for (let skill of skillList) {
+            skillMap[skill.name] = skill;
+        }
+        return skillMap;
+    }
+
+    /* A base-level skill, i.e., Basic Artillery and the like. */
+    /* TODO: non-working at this point. */
+    isBaseSkill(skill) {
+        if (skill.skill_cost_1 === null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+     * If character has the skill, use the check directly.
+     *
+     * If character does not have the skill, but the skill level 0
+     * has cost of 0, use level 0 check.  This should use the normal
+     * skill check calculation, as the character may have armor
+     * or edges which modify the skill check.
+     *
+     * If character doesn't have the skill, and the skill level 0 has
+     * a non-zero cost, calculate check defaulted to half-ability.
+     */
+    skillCheck(skillName, stat) {
+        var skill = this.state.skillMap[skillName];
+        if (this.isBaseSkill(skill)) {
+            return null;
+        }
+
+        if (!stat) {
+            stat = skill.stat;
+        }
+        var ability = this.props.stats[stat.toLowerCase()];
+        var level = this.skillLevel(skillName);
+        if (level === "U") {
+            return Math.round(ability / 4)
+        } else if (level === "B") {
+            return Math.round(ability / 2)
+        } else {
+            return ability + level * 5;
+        }
+    }
+
+    /* U is quarter-skill, i.e., using a pistol even without Basic
+       Firearms.  B is half-skill, i.e., the character has top-level skill,
+       but not the skill required.  Otherwise, if the character has the
+        skill, return the level of the skill. */
+    skillLevel(skillName) {
+        var cs = this.state.characterSkillMap[skillName];
+        var skill = this.state.skillMap[skillName];
+        if (!cs) {
+            if (skill.required_skills.length > 0) {
+                for (let reqd of skill.required_skills) {
+                    if (!(reqd in this.state.characterSkillMap)) {
+                        return "U";
+                    }
+                }
+            }
+            if (skill.skill_cost_0 > 0) {
+                return 'B';
+            } else {
+                return 0;
+            }
+        } else {
+            return cs.level;
+        }
+    }
+
+}
+
+export default SkillHandler;
---
+++ sheet/static/react/__tests__/skill-handler-tests.js
@@ -0,0 +1,67 @@
+jest.dontMock('../SkillHandler');
+jest.dontMock('../sheet-util');
+jest.dontMock('./factories')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+var factories = require('./factories');
+
+const SkillHandler = require('../SkillHandler').default;
+
+describe('SkillHandler', function() {
+    "use strict";
+
+    var getSkillHandler = function (givenProps) {
+        var props = {
+            characterSkills: [factories.characterSkillFactory({
+                skill: "Pistol",
+                level: 1
+            }),
+            factories.characterSkillFactory({
+                skill: "Basic Firearms",
+                level: 0
+            })],
+            allSkills: [
+                factories.skillFactory({
+                    name: "Pistol", stat: "DEX",
+                    required_skills: ["Basic Firearms"]
+                }),
+                factories.skillFactory({name: "Basic Firearms", stat: "INT"})
+            ],
+            stats: factories.statsFactory({dex: 50})
+        };
+        if (typeof(givenProps) !== "undefined") {
+            props = Object.assign(props, givenProps);
+        }
+
+        return new SkillHandler(props);
+    };
+
+    it('calculates skill level', function () {
+        var handler = getSkillHandler();
+
+        expect(handler.skillLevel("Pistol")).toEqual(1);
+    });
+
+    it('calculates skill check', function () {
+        var handler = getSkillHandler();
+        expect(handler.skillCheck("Pistol")).toEqual(55);
+    });
+
+    it('calculates unskilled check', function () {
+        var handler = getSkillHandler({characterSkills: [
+            factories.characterSkillFactory({
+                skill: "Basic Firearms",
+                level: 0
+            })]});
+        expect(handler.skillCheck("Pistol")).toEqual(25);
+    });
+
+    it('calculates extremely unskilled check', function () {
+        var handler = getSkillHandler({characterSkills: []});
+        expect(handler.skillCheck("Pistol")).toEqual(13);
+    });
+
+});
