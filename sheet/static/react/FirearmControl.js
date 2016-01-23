252b4869817b83f4501cf05ea47f08392df0d467
Sami J. Lehtinen <sjl@iki.fi>
2016-01-24 01:39:57 +0200

    WIP on firearms.  Skillchecks and initiatives.

---
+++ sheet/static/react/FirearmControl.js
@@ -0,0 +1,146 @@
+import React from 'react';
+var util = require('sheet-util');
+
+class FirearmControl extends React.Component {
+    skillCheck () {
+        var check = this.props.skillHandler.skillCheck(
+            this.props.weapon.base.base_skill);
+
+        /* TODO: This works differently with ranged and close-combat
+           weapons (CCV on unskilled).  Might need a slightly more intricate
+           system to handle this. */
+        if (this.props.weapon.base.skill) {
+            /* Note that the check cannot be inverted here, as the > 0
+               will return false also if skill is null, undefined or a
+               string, which would also apply if the comparison was < 0. */
+            if (!(this.props.skillHandler.skillLevel(
+                    this.props.weapon.base.skill) > 0)) {
+                check -= 10;
+            }
+        }
+        if (this.props.weapon.base.skill2) {
+            /* Note that the check cannot be inverted here, as the > 0
+               will return false also if skill is null, undefined or a
+               string, which would also apply if the comparison was < 0. */
+            if (!(this.props.skillHandler.skillLevel(
+                    this.props.weapon.base.skill2)> 0)) {
+                check -= 10;
+            }
+        }
+        return check;
+    }
+
+    rof() {
+        var ammo = this.props.weapon.ammo;
+        var base = this.props.weapon.base;
+        var impulse = (parseFloat(ammo.weight) *
+            parseFloat(ammo.velocity))/1000;
+
+        var recoil = impulse / (parseFloat(base.duration) *
+            parseFloat(base.stock) *
+            (parseFloat(base.weight) + 6));
+        return 30 / (recoil + parseFloat(base.weapon_class_modifier));
+    }
+
+    static checkMod(roa, act, baseBonus, extraActionModifier) {
+        if (1/act >= 1/roa + 1) {
+            return baseBonus;
+        }
+        if (act < 0.5 * roa) {
+            return roa / act;
+        }
+        /* Gap.*/
+        if (act > roa) {
+            return - act/roa * 20 + extraActionModifier;
+        }
+
+        // Value in the gap.
+        return 0;
+    }
+
+    static counterPenalty(modifier, stat) {
+        if (modifier > 0) {
+            /* Not a penalty, a bonus. */
+            return modifier;
+        }
+        return Math.min(0, modifier + util.rounddown((stat - 45)/3))
+    }
+
+    getStat(stat) {
+        return this.props.skillHandler.getStat(stat);
+    }
+
+    skillChecks(actions) {
+        var rof = this.rof();
+        var baseCheck = this.skillCheck();
+
+        var checks = [];
+
+        for (let act of actions) {
+            if (act > 2 * rof) {
+                checks.push(null);
+            } else {
+                checks.push(
+                    FirearmControl.counterPenalty(
+                        Math.round(FirearmControl.checkMod(rof, act, 10, 15)),
+                        this.getStat("FIT")) +
+                    baseCheck);
+            }
+        }
+        return checks;
+    }
+
+    initiatives(actions) {
+        var baseIMultipliers = [1, 4, 7, 2, 5, 8, 3, 6, 9];
+        var rof = this.rof();
+        var baseI = -5 / rof;
+        var readiedBaseI = -1;
+        var base = this.props.weapon.base;
+        var targetI = base.target_initiative;
+        var initiative = this.getStat('ref') / 10 +
+            this.getStat('int') / 20 +
+            this.getStat('psy') / 20;
+
+        var initiatives = [];
+        for (let act of actions) {
+            if (act > 2 * rof) {
+                initiatives.push(null);
+            } else {
+                if (rof > 2 * act && act < 1) {
+                    /* Assuming multi-turn action, where readying of the
+                       weapon is possible and target has already been
+                       acquired.  House Rules, initiative, p. 8. */
+                    initiatives.push(
+                        Math.max(readiedBaseI, baseI) +
+                        Math.min(targetI + 3, 0));
+                } else {
+                    /* One target acquire is assumed for the rest of the
+                       initiatives.  If target is changed, target-I should
+                       be added to the rest of the initiatives.
+
+                       TODO: add note to sheet about this.
+                     */
+                    initiatives.push(
+                        baseIMultipliers[Math.ceil(act) - 1] * baseI +
+                        targetI);
+                }
+            }
+        }
+        return initiatives.map(function (el) {
+            if (el !== null) {
+                return Math.round(el + initiative);}
+            else { return null }
+        });
+    }
+
+    render () {
+        return <div></div>;
+    }
+}
+
+FirearmControl.props = {
+    skillHandler: React.PropTypes.object.isRequired,
+    weapon: React.PropTypes.object.isRequired
+};
+
+export default FirearmControl;
---
+++ sheet/static/react/__tests__/firearm-control-tests.js
@@ -0,0 +1,148 @@
+jest.dontMock('../FirearmControl');
+jest.dontMock('../SkillHandler');
+jest.dontMock('../sheet-util');
+jest.dontMock('./factories');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+const FirearmControl = require('../FirearmControl').default;
+const SkillTable = require('../SkillTable').default;
+const SkillHandler = require('../SkillHandler').default;
+
+var rest = require('sheet-rest');
+
+var factories = require('./factories');
+
+describe('FirearmControl', function() {
+    "use strict";
+
+    var getFirearmControl = function (givenProps) {
+        var handlerProps = {
+            characterSkills: [],
+            allSkills: [
+                factories.skillFactory({
+                    name: "Pistol", stat: "DEX",
+                    required_skills: ["Basic Firearms"]
+                }),
+                factories.skillFactory({
+                    name: "Basic Firearms",
+                    stat: "DEX"
+                }),
+                factories.skillFactory({
+                    name: "Wheeled",
+                    stat: "DEX"
+                })
+            ],
+            stats: {dex: 45}
+        };
+        if (givenProps && 'handlerProps' in givenProps) {
+            handlerProps = Object.assign(handlerProps,
+                givenProps.handlerProps);
+            delete givenProps.handlerProps;
+        }
+        handlerProps.stats = factories.statsFactory(handlerProps.stats);
+
+        var props = {
+            weapon: factories.firearmFactory({base: {base_skill: "Pistol"}}),
+            skillHandler: new SkillHandler(handlerProps)
+        };
+        if (givenProps) {
+            props = Object.assign(props, givenProps);
+        }
+        var table = TestUtils.renderIntoDocument(
+            <FirearmControl {...props}/>
+        );
+
+        return TestUtils.findRenderedComponentWithType(table,
+            FirearmControl);
+    };
+
+    it("can calculate single fire skill checks", function () {
+        var firearm = getFirearmControl();
+        expect(firearm.skillCheck()).toEqual(11);
+    });
+
+    it("can calculate effect of missing specialization fire skill checks",
+        function () {
+        var firearm = getFirearmControl({
+            handlerProps: {
+                characterSkills: [factories.characterSkillFactory({
+                    skill: "Pistol",
+                    level: 1
+                })]
+            },
+            weapon: factories.firearmFactory({base: {base_skill: "Pistol", skill: "Wheeled"}})});
+        expect(firearm.skillCheck()).toEqual(40);
+    });
+
+    it("notices specializations", function () {
+        var firearm = getFirearmControl({
+            handlerProps: {
+                characterSkills: [factories.characterSkillFactory({
+                    skill: "Pistol",
+                    level: 1
+                }),
+                    factories.characterSkillFactory({
+                        skill: "Wheeled",
+                        level: 1
+                    })]
+            },
+            weapon: factories.firearmFactory({base: {base_skill: "Pistol", skill: "Wheeled"}})});
+        expect(firearm.skillCheck()).toEqual(50);
+    });
+
+    it ("calculates correct ROF", function() {
+        var firearm = getFirearmControl({
+            handlerProps: {
+                characterSkills: [factories.characterSkillFactory({
+                    skill: "Pistol",
+                    level: 0
+                })]},
+            weapon: factories.firearmFactory({base: {base_skill: "Pistol"}})
+        });
+        expect(firearm.rof()).toBeCloseTo(2.86, 2);
+    });
+
+    it ("can calculate a row of checks", function() {
+        var firearm = getFirearmControl({
+            handlerProps: {
+                characterSkills: [factories.characterSkillFactory({
+                    skill: "Pistol",
+                    level: 0
+                })]},
+            weapon: factories.firearmFactory({base: {base_skill: "Pistol"}})
+        });
+        expect(firearm.skillChecks([0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+            .toEqual([55, 48, 45, 39, 32, 25, null, null, null, null]);
+    });
+
+    it ("takes into account penalties countered", function() {
+        var firearm = getFirearmControl({
+            handlerProps: {
+                characterSkills: [factories.characterSkillFactory({
+                    skill: "Pistol",
+                    level: 0
+                })],
+                stats: {dex: 45, fit: 63}
+            },
+            weapon: factories.firearmFactory({base: {base_skill: "Pistol"}})
+        });
+        expect(firearm.skillChecks([0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+            .toEqual([55, 48, 45, 45, 38, 31, null, null, null, null]);
+    });
+
+    it ("can calculate a row of initiatives", function() {
+        var firearm = getFirearmControl({
+            handlerProps: {
+                characterSkills: [factories.characterSkillFactory({
+                    skill: "Pistol",
+                    level: 0
+                })]},
+            weapon: factories.firearmFactory({base: {base_skill: "Pistol"}})
+        });
+        expect(firearm.initiatives([0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+            .toEqual([8, 6, 1, -4, 5, -1, null, null, null, null]);
+    });
+
+});
