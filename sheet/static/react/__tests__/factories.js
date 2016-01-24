f7173e180a8dc3257123a297e530ad7566b04ce3
Sami J. Lehtinen <sjl@iki.fi>
2016-01-24 16:13:42 +0200

    Added firearmFactory.

--- sheet/static/react/__tests__/factories.js
+++ sheet/static/react/__tests__/factories.js
@@ -1,6 +1,16 @@
 var statsFactory = function (overrideStats) {
     var _baseStats = {
-        cha: 45
+        fit: 45,
+        ref: 45,
+        lrn: 45,
+        int: 45,
+        wil: 45,
+        psy: 45,
+        cha: 45,
+        pos: 45,
+        dex: 45,
+        mov: 45,
+        imm: 45
     };
     return Object.assign(_baseStats, overrideStats);
 };
@@ -43,6 +53,67 @@ var characterSkillFactory = function (overrideFields) {
     return newSkill
 };
 
+var firearmFactory = function (overrideFields) {
+    "use strict";
+    var firearm ={
+        "id": 5,
+        "base": {
+            "name": "Glock 19",
+            "short_name": "",
+            "description": "",
+            "notes": "",
+            "draw_initiative": -3,
+            "durability": 4,
+            "dp": 3,
+            "weight": "0.7",
+            "target_initiative": -1,
+            "range_pb": null, "range_xs": null, "range_vs": null,
+            "range_s": 12, "range_m": 24, "range_l": 48,
+            "range_xl": null, "range_e": null,
+            "autofire_rpm": null,
+            "autofire_class": "",
+            "sweep_fire_disabled": false,
+            "restricted_burst_rounds": 0,
+            "stock": "1.00",
+            "duration": "0.110",
+            "weapon_class_modifier": "6.00",
+            "tech_level": 4,
+            "base_skill": "Handguns",
+            "skill": null,
+            "skill2": null
+        },
+        "ammo": {
+            "id": 256,
+            "num_dice": 1,
+            "dice": 6,
+            "extra_damage": 0,
+            "leth": 5,
+            "plus_leth": 0,
+            "label": "9Pb",
+            "type": "P",
+            "bullet_type": "FMJ",
+            "bypass": 0,
+            "weight": "7.500",
+            "velocity": 440,
+            "tech_level": 4
+        }
+    };
+
+    var overrides = Object.assign({}, overrideFields ? overrideFields : {});
+    if ('ammo' in overrides) {
+        firearm.ammo = Object.assign(firearm.ammo, overrideFields.ammo);
+        delete overrides.ammo;
+    }
+    if ('base' in overrides) {
+        firearm.base = Object.assign(firearm.base, overrideFields.base);
+        delete overrides.base;
+    }
+
+    return Object.assign(firearm, overrides);
+};
+
 module.exports = {characterSkillFactory: characterSkillFactory,
     skillFactory: skillFactory,
-    statsFactory: statsFactory}
+    statsFactory: statsFactory,
+    firearmFactory: firearmFactory
+};
