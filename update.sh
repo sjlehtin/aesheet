4a8cef363a9a3650e61cd856d4b1d42e0062829c
Sami J. Lehtinen <sjl@ssh.com>
2016-11-08 23:25:23 +0200

    Fixed.

--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 set -e
+set -x
 
 . ve/bin/activate
 
 python manage.py migrate
 (cd sheet/static/react && ./node_modules/.bin/webpack)
-yes | python manage.py collectstatic --ignore node_modules
+python manage.py collectstatic --ignore node_modules --noinput
 
 # sudo supervisorctl restart devsheet
