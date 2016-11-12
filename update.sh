c2650f722a161f60183ca0a06492d512af067cf9
Sami J. Lehtinen <sjl@ssh.com>
2016-11-12 19:16:28 +0200

    Do not back out due to prompts.

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ set -x
 
 python manage.py migrate
 (cd sheet/static/react && ./node_modules/.bin/webpack)
-python manage.py collectstatic --ignore node_modules --noinput
+yes yes | python manage.py collectstatic --ignore node_modules
 
 # sudo supervisorctl restart devsheet
