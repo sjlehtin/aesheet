7505d77dfb370bf8b91ba85d28b8d375309ecbe7
Sami J. Lehtinen <sjl@ssh.com>
2016-11-20 19:13:08 +0200

    Fixed to use factories.skillHandlerFactory.

--- sheet/static/react/__tests__/skill-handler-tests.js
+++ sheet/static/react/__tests__/skill-handler-tests.js
@@ -8,6 +8,7 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
 var factories = require('./factories');
+var util = require('../sheet-util');
 
 const StatHandler = require('../StatHandler').default;
 const SkillHandler = require('../SkillHandler').default;
@@ -15,58 +16,38 @@ const SkillHandler = require('../SkillHandler').default;
 describe('SkillHandler', function() {
     "use strict";
 
-    var getSkillHandler = function (givenProps) {
-        var props = {
-            characterSkills: [factories.characterSkillFactory({
-                skill: "Pistol",
-                level: 1
-            }),
-            factories.characterSkillFactory({
-                skill: "Basic Firearms",
-                level: 0
-            })],
-            allSkills: [
-                factories.skillFactory({
-                    name: "Pistol", stat: "DEX",
-                    required_skills: ["Basic Firearms"]
-                }),
-                factories.skillFactory({name: "Basic Firearms", stat: "INT"})
-            ],
-            stats: new StatHandler({
-                character: factories.characterFactory({
-                    cur_ref: 50, cur_int: 50}),
-                edges: [], effects: []})
-        };
-        if (typeof(givenProps) !== "undefined") {
-            props = Object.assign(props, givenProps);
-        }
-
-        return new SkillHandler(props);
-    };
+    var getSkillHandler = factories.skillHandlerFactory;
 
     it('calculates skill level', function () {
-        var handler = getSkillHandler();
+        var handler = getSkillHandler({skills: [
+            {skill: "Pistol", level: 1}]});
 
         expect(handler.skillLevel("Pistol")).toEqual(1);
     });
 
     it('calculates skill check', function () {
-        var handler = getSkillHandler();
-        expect(handler.skillCheck("Pistol")).toEqual(55);
+        var handler = getSkillHandler({skills: [
+            {skill: "Pistol", level: 1}]});
+        expect(handler.skillCheck("Pistol")).toEqual(48);
     });
 
     it('calculates unskilled check', function () {
-        var handler = getSkillHandler({characterSkills: [
-            factories.characterSkillFactory({
-                skill: "Basic Firearms",
-                level: 0
-            })]});
-        expect(handler.skillCheck("Pistol")).toEqual(25);
+        var handler = getSkillHandler({
+            skills: [{skill: {name: "Basic Firearms", skill_cost_1: null}, level: 0}],
+            allSkills: [{
+                name: "Pistol", required_skills: ["Basic Firearms"]}]
+        });
+        expect(handler.skillCheck("Pistol")).toEqual(util.roundup(43/2));
     });
 
     it('calculates extremely unskilled check', function () {
-        var handler = getSkillHandler({characterSkills: []});
-        expect(handler.skillCheck("Pistol")).toEqual(13);
+        var handler = getSkillHandler({
+            allSkills: [{
+                name: "Pistol",
+                required_skills: ["Basic Firearms"]},
+                {name: "Basic Firearms"}]
+        });
+        expect(handler.skillCheck("Pistol")).toEqual(util.roundup(43/4));
     });
 
 });
