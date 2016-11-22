f0076c2675ad5275e8d6b9fb12ced21bdca5436a
Sami J. Lehtinen <sjl@ssh.com>
2016-11-23 01:04:24 +0200

    stats property not used anymore.

--- sheet/static/react/__tests__/skill-row-tests.js
+++ sheet/static/react/__tests__/skill-row-tests.js
@@ -52,7 +52,6 @@ describe('SkillRow', function() {
             }),
 
             characterSkill: characterSkillFactory({level: 1}),
-            stats: statsFactory({cha: 55}),
             skill: skillFactory({stat: "CHA"})});
         expect(row.skillCheck()).toEqual(60);
     });
@@ -67,7 +66,6 @@ describe('SkillRow', function() {
                         level: 0}],
             }),
             characterSkill: characterSkillFactory({level: 0}),
-            stats: statsFactory({cha: 55}),
             skill: skillFactory({stat: "CHA",
             skill_cost_1: null,
                 skill_cost_2: null,
@@ -84,7 +82,6 @@ describe('SkillRow', function() {
                 allSkills: [{skill: "Acting / Bluff", stat: "CHA"}]
             }),
 
-            stats: statsFactory({cha: 55}),
             skill: skillFactory({stat: "CHA"})});
         expect(row.skillCheck()).toEqual(28);
     });
@@ -99,7 +96,6 @@ describe('SkillRow', function() {
                     skill_cost_0: 0,
                     stat: "CHA"}]
             }),
-            stats: statsFactory({cha: 55}),
             skill: skillFactory({stat: "CHA", skill_cost_0: 0})});
         expect(row.skillCheck()).toEqual(55);
     });
@@ -113,7 +109,6 @@ describe('SkillRow', function() {
             }),
 
             characterSkill: characterSkillFactory({level: 1}),
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA"})
         });
         expect(row.skillCheck("wil")).toEqual(65);
@@ -130,7 +125,6 @@ describe('SkillRow', function() {
             characterSkill: characterSkillFactory({
                 skill: "Balance",
                 level: 1}),
-            stats: statsFactory({mov: 45, ref: 60}),
             skill: skillFactory({name: "Balance", stat: "MOV"}),
             renderForStats: ["mov", "ref"]
         });
@@ -154,7 +148,6 @@ describe('SkillRow', function() {
             }),
 
             characterSkill: cs,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA"}),
             onCharacterSkillModify: spy
         });
@@ -172,7 +165,6 @@ describe('SkillRow', function() {
             }),
 
             characterSkill: cs,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA"}),
             onCharacterSkillModify: spy
         });
@@ -189,7 +181,6 @@ describe('SkillRow', function() {
             }),
 
             characterSkill: undefined,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA"}),
             onCharacterSkillModify: spy
         });
@@ -207,7 +198,6 @@ describe('SkillRow', function() {
             }),
 
             characterSkill: cs,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA", min_level: 1}),
             onCharacterSkillModify: spy
         });
@@ -222,7 +212,6 @@ describe('SkillRow', function() {
                 allSkills: [{name: "Balance"}]
             }),
             characterSkill: undefined,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA"}),
             onCharacterSkillModify: spy
         });
@@ -240,7 +229,6 @@ describe('SkillRow', function() {
             }),
 
             characterSkill: cs,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA", max_level: 3}),
             onCharacterSkillModify: spy
         });
@@ -256,7 +244,6 @@ describe('SkillRow', function() {
             }),
 
             characterSkill: cs,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA", max_level: 3}),
         });
         var node = ReactDOM.findDOMNode(row);
@@ -273,7 +260,6 @@ describe('SkillRow', function() {
                 allSkills: [{name: "Frozzling"}]
             }),
             characterSkill: cs,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA", max_level: 3}),
         });
         var node = ReactDOM.findDOMNode(row);
@@ -290,7 +276,6 @@ describe('SkillRow', function() {
                 allSkills: [{name: "Frozzling"}, {name: "Foobying"}]
             }),
             characterSkill: cs,
-            stats: statsFactory({cha: 45, wil: 60}),
             skill: skillFactory({stat: "CHA", max_level: 3}),
         });
         var node = ReactDOM.findDOMNode(row);
