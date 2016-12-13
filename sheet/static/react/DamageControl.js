6562817d1af47040841f773b87673a0fc179f4a7
Sami J. Lehtinen <sjl@ssh.com>
2016-12-13 13:56:56 +0200

    Narrowed other fields, effect should fit better.

--- sheet/static/react/DamageControl.js
+++ sheet/static/react/DamageControl.js
@@ -138,7 +138,7 @@ class DamageControl extends React.Component {
             />;});
         var wounds = <Table>
                 <thead>
-                <tr><th>Loc</th><th>Type</th><th>Dmg</th><th>Effect</th></tr>
+                <tr><th style={{width: "12em"}}>Loc</th><th style={{width: "8em"}}>Type</th><th style={{width: "5em"}}>Dmg</th><th>Effect</th></tr>
                 </thead>
                 <tbody>
                 {rows}
