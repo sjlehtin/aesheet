ced748604acf0f685f594f5be2308e676f93d92e
Sami J. Lehtinen <sjl@ssh.com>
2016-12-24 01:34:50 +0200

    Updated with new node_modules location.

--- update.sh
+++ update.sh
@@ -6,7 +6,9 @@ set -x
 . ve/bin/activate
 
 python manage.py migrate
-(cd sheet/static/react && ./node_modules/.bin/webpack)
+
+./node_modules/.bin/webpack
+
 yes yes | python manage.py collectstatic --ignore node_modules
 
 # sudo supervisorctl restart devsheet
