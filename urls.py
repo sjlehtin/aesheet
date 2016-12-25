84a548339d7eecd8087cbc93eb3591f882df0850
Sami J. Lehtinen <sjl@ssh.com>
2016-12-25 23:03:40 +0200

    Fixed reverse issue with login page.
    Root cause probably upgrade to Django 1.10, but I did not verify.

--- urls.py
+++ urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^accounts/login/$', django.contrib.auth.views.login),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name="logout"),
 
     url(r'^accounts/profile/$', views.profile, name="profile"),
