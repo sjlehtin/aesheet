0c7ba828541be63aea2a431665221122d38a8832
Sami J. Lehtinen <sjl@ssh.com>
2016-12-25 19:59:33 +0200

    Moved sheet organizations test to views tests.

--- sheet/tests/test_unsorted.py
+++ sheet/tests/test_unsorted.py
@@ -106,48 +106,6 @@ class Views(WebTest):
         self.assertContains(response, "Priest")
 
 
-class SheetOrganizationTestCase(TestCase):
-    def setUp(self):
-        factories.SheetFactory(character__name="Martel",
-                               character__campaign__name="FRP")
-        factories.SheetFactory(character__name="Yukaghir",
-                               character__campaign__name="FRP")
-        factories.SheetFactory(character__name="Asa",
-                               character__campaign__name="MR")
-        self.admin = factories.UserFactory(username='admin')
-        self.assertTrue(self.client.login(username='admin',
-                                          password='foobar'))
-
-    def test_sheet_organization(self):
-        campaigns = Sheet.get_by_campaign(self.admin)
-        self.assertListEqual([u"FRP", u"MR"],
-                             [campaign.name for campaign in campaigns])
-        self.assertListEqual([u"Martel", u"Yukaghir", u"Asa"],
-                             [sheet.character.name for campaign in campaigns
-                              for sheet in campaign.objects])
-
-    def test_character_organization(self):
-        campaigns = Character.get_by_campaign(self.admin)
-        self.assertListEqual(["FRP", "MR"],
-                             [campaign.name for campaign in campaigns])
-        self.assertListEqual(["Martel", "Yukaghir", "Asa"],
-                             [character.name for campaign in campaigns
-                              for character in campaign.objects])
-
-    def test_character_view(self):
-        response = self.client.get(reverse('characters_index'))
-        # Verify the headings are present.
-        self.assertContains(response, 'FRP')
-        self.assertContains(response, 'MR')
-
-    def test_sheet_view(self):
-        response = self.client.get(reverse('sheets_index'))
-
-        # Verify the headings are present.
-        self.assertContains(response, 'FRP')
-        self.assertContains(response, 'MR')
-
-
 class CreateURLTestCase(TestCase):
     def setUp(self):
         factories.UserFactory(username="admin")
--- sheet/tests/test_views.py
+++ sheet/tests/test_views.py
@@ -5,6 +5,7 @@ from __future__ import division
 import logging
 
 from django.test import TestCase
+from sheet.models import Sheet, Character
 
 from django.core.urlresolvers import reverse
 import sheet.factories as factories
@@ -23,3 +24,45 @@ class CharactersViewTestCase(TestCase):
     def test_view(self):
         response = self.client.get(reverse('characters_index'))
         self.assertIn("Örkki", response.content)
+
+
+class SheetOrganizationTestCase(TestCase):
+    def setUp(self):
+        factories.SheetFactory(character__name="Martel",
+                               character__campaign__name="FRP")
+        factories.SheetFactory(character__name="Yukaghir",
+                               character__campaign__name="FRP")
+        factories.SheetFactory(character__name="Asa",
+                               character__campaign__name="MR")
+        self.admin = factories.UserFactory(username='admin')
+        self.assertTrue(self.client.login(username='admin',
+                                          password='foobar'))
+
+    def test_sheet_organization(self):
+        campaigns = Sheet.get_by_campaign(self.admin)
+        self.assertListEqual([u"FRP", u"MR"],
+                             [campaign.name for campaign in campaigns])
+        self.assertListEqual([u"Martel", u"Yukaghir", u"Asa"],
+                             [sheet.character.name for campaign in campaigns
+                              for sheet in campaign.objects])
+
+    def test_character_organization(self):
+        campaigns = Character.get_by_campaign(self.admin)
+        self.assertListEqual(["FRP", "MR"],
+                             [campaign.name for campaign in campaigns])
+        self.assertListEqual(["Martel", "Yukaghir", "Asa"],
+                             [character.name for campaign in campaigns
+                              for character in campaign.objects])
+
+    def test_character_view(self):
+        response = self.client.get(reverse('characters_index'))
+        # Verify the headings are present.
+        self.assertContains(response, 'FRP')
+        self.assertContains(response, 'MR')
+
+    def test_sheet_view(self):
+        response = self.client.get(reverse('sheets_index'))
+
+        # Verify the headings are present.
+        self.assertContains(response, 'FRP')
+        self.assertContains(response, 'MR')
