c4ef495aa0af205d6088639759fa1edec531d137
Sami J. Lehtinen <sjl@ssh.com>
2016-12-25 02:01:00 +0200

    AmmoControl can change ammunition.
    Integration to top-level components still missing.

--- react/AmmoControl.js
+++ react/AmmoControl.js
@@ -1,14 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Button} from 'react-bootstrap';
+import DropdownList from 'react-widgets/lib/DropdownList';
+import AddFirearmControl from './AddFirearmControl';
+
+const rest = require('./sheet-rest');
+
 class AmmoControl extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            busy: false,
+            editing: false,
+            open: true,
+            ammoChoices: [],
+            selectedAmmo: undefined
+        };
+    }
+
+    updateAmmoSelection() {
+        return rest.getData(this.props.url).then(json => {
+            let selectedAmmo = json.find((el) => {return this.props.ammo.id === el.id});
+
+            this.setState({busy: false, ammoChoices: json,
+                           selectedAmmo: selectedAmmo});
+        });
+    }
+
+    handleChangeClick() {
+        this.setState({busy: true, editing: true});
+
+        this.updateAmmoSelection();
+    }
+
+    cancelEdit() {
+        this.setState({editing: false,
+                       selectedAmmo: undefined});
+    }
+
+    handleKeyDown(e) {
+        if (e.keyCode === 27) {
+            /* Escape. */
+            this.cancelEdit();
+        }
+    }
+
+    handleChange(value) {
+        this.setState({selectedAmmo: value});
+    }
+
+    isValid() {
+        return this.state.ammoChoices.indexOf(this.state.selectedAmmo) >= 0;
+    }
+
+    handleSubmit() {
+        if (this.props.onChange && this.isValid()) {
+            this.props.onChange(this.state.selectedAmmo).then(
+                () => this.cancelEdit());
+        }
+    }
+
     render() {
-        return <div>{this.props.ammo.label} {this.props.ammo.bullet_type}</div>;
+        let content;
+        if (this.state.editing) {
+            content = <div><DropdownList
+                                defaultValue={this.props.ammo}
+                                open={this.state.open}
+                                value={this.state.selectedAmmo}
+                                busy={this.state.busy}
+                                textField={(obj) => {return AddFirearmControl.formatAmmo(obj);}}
+                                onChange={(value) => this.handleChange(value)}
+                                onToggle={() => this.setState({open: !this.state.open})}
+                                filter="contains"
+                                ref={c => {if (c) ReactDOM.findDOMNode(c).focus() }}
+                                data={this.state.ammoChoices}
+
+            />
+            <Button onClick={(e) => this.handleSubmit()}
+                                ref={(c) => this._submitButton = c}
+                                bsSize="xsmall">Change</Button>
+            </div>;
+        } else {
+            content = <div><span>{this.props.ammo.label} {this.props.ammo.bullet_type}</span>
+            <Button onClick={(e) => this.handleChangeClick()}
+                                ref={(c) => this._changeButton = c}
+                                bsSize="xsmall">Change</Button>
+            </div>;
+        }
+        return <div onKeyDown={this.handleKeyDown.bind(this)}>
+            {content}
+        </div>;
     }
 }
 
 AmmoControl.props = {
-    ammo: React.PropTypes.object.isRequired
+    ammo: React.PropTypes.object.isRequired,
+    url: React.PropTypes.string,
+    onChange: React.PropTypes.func
 };
 
 export {AmmoControl};
--- react/__tests__/ammo-control-tests.js
+++ react/__tests__/ammo-control-tests.js
@@ -25,32 +25,127 @@ describe('AmmoControl', () => {
         expect(ReactDOM.findDOMNode(control).textContent).toContain('AP-HP');
     });
 
+    it('contains a change button', () => {
+        const control = getAmmoControl({ammo: {label: 'Skubadoo', bullet_type: "AP-HP"},
+                                        url: "/rest/foo"});
+        expect(control._changeButton).toBeDefined();
+    });
+
     it('on pressing change, it loads the ammo selection', () => {
-        const getPromise = Promise.resolve([]);
+
+        let ammo = factories.ammunitionFactory();
+        const getPromise = Promise.resolve([ammo]);
         rest.getData.mockClear();
         rest.getData.mockReturnValue(getPromise);
 
-        const control = getAmmoControl({ammo: {label: 'Skubadoo', bullet_type: "AP-HP",
-                                        url: "/rest/foo"}});
+        const control = getAmmoControl({ammo: {label: 'Skubadoo', bullet_type: "AP-HP"},
+                                        url: "/rest/foo"});
 
-        // expect(control._changeAmmoButton).toBeDefined();
+        expect(control._changeButton).toBeDefined();
+
+        TestUtils.Simulate.click(ReactDOM.findDOMNode(control._changeButton));
+
+        expect(control.state.editing).toBe(true);
+        expect(control.state.busy).toBe(true);
+
+        expect(rest.getData).toBeCalledWith('/rest/foo');
 
         return getPromise.then(() => {
-            console.log("Promise resolved");
-            // fail("foo?");
+            expect(control.state.busy).toBe(false);
+            expect(control.state.ammoChoices).toEqual([ammo]);
         });
     });
 
-    xit('on pressing change, preselects current ammo', () => {
+    it('on pressing change, preselects current ammo', () => {
+        let ammo = factories.ammunitionFactory({id: 42});
+        const getPromise = Promise.resolve([
+            factories.ammunitionFactory({id: 12}),
+            Object.assign({}, ammo), // Ensure distinct, but equal otherwise, object.
+            factories.ammunitionFactory({id: 50})]);
+        rest.getData.mockClear();
+        rest.getData.mockReturnValue(getPromise);
+
+        const control = getAmmoControl({ammo: ammo, url: "/rest/foo"});
+
+        TestUtils.Simulate.click(ReactDOM.findDOMNode(control._changeButton));
+
+        return getPromise.then(() => {
+            expect(control.state.selectedAmmo).toEqual(ammo);
+        });
 
     });
 
-    xit('allows canceling change with esc', () => {
+    it('allows canceling change with esc', () => {
+        const control = getAmmoControl();
 
+        TestUtils.Simulate.click(ReactDOM.findDOMNode(control._changeButton));
+
+        expect(control.state.editing).toBe(true);
+
+        TestUtils.Simulate.keyDown(
+            ReactDOM.findDOMNode(control),
+            {key: "Esc", keyCode: 27, which: 27});
+        expect(control.state.editing).toBe(false);
     });
 
-    xit('allows changing the ammo', () => {
+    it('on canceling edit, clears previously selected ammo', () => {
+        let ammo = factories.ammunitionFactory({id: 42});
+        let newAmmo = factories.ammunitionFactory({id: 12});
+        const getPromise = Promise.resolve([
+            newAmmo,
+            Object.assign({}, ammo), // Ensure distinct, but equal otherwise, object.
+            factories.ammunitionFactory({id: 50})]);
 
+        rest.getData.mockClear();
+        rest.getData.mockReturnValue(getPromise);
+
+        const control = getAmmoControl({ammo: ammo, url: "/rest/foo"});
+
+        TestUtils.Simulate.click(ReactDOM.findDOMNode(control._changeButton));
+
+        return getPromise.then(() => {
+            control.handleChange(newAmmo);
+
+            TestUtils.Simulate.keyDown(
+                ReactDOM.findDOMNode(control),
+                {key: "Esc", keyCode: 27, which: 27});
+            expect(control.state.selectedAmmo).not.toBeDefined();
+        });
+    });
+
+    it('allows changing the ammo', () => {
+        let ammo = factories.ammunitionFactory({id: 42});
+        let newAmmo = factories.ammunitionFactory({id: 12});
+        const getPromise = Promise.resolve([
+            newAmmo,
+            Object.assign({}, ammo), // Ensure distinct, but equal otherwise, object.
+            factories.ammunitionFactory({id: 50})]);
+
+        rest.getData.mockClear();
+        rest.getData.mockReturnValue(getPromise);
+
+        let spy = jest.fn();
+        let updatePromise = Promise.resolve({});
+        spy.mockReturnValue(updatePromise);
+
+        const control = getAmmoControl({ammo: ammo, url: "/rest/foo",
+            onChange: spy });
+
+        TestUtils.Simulate.click(ReactDOM.findDOMNode(control._changeButton));
+
+        return getPromise.then(() => {
+            expect(control.state.selectedAmmo).toEqual(ammo);
+
+            control.handleChange(newAmmo);
+
+            TestUtils.Simulate.click(ReactDOM.findDOMNode(control._submitButton));
+
+            expect(spy).toBeCalledWith(newAmmo);
+
+            return updatePromise.then(() => {
+                expect(control.state.editing).toEqual(false);
+            });
+        });
     });
 
 });
