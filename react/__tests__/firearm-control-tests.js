4cbcaddfc5e95b3257ea17d9b01e0d863c80285a
Sami J. Lehtinen <sjl@ssh.com>
2016-12-26 16:42:17 +0200

    Fixed PyCharm JS warnings.

--- react/__tests__/firearm-control-tests.js
+++ react/__tests__/firearm-control-tests.js
@@ -8,18 +8,16 @@ const SkillHandler = require('../SkillHandler').default;
 
 const factories = require('./factories');
 
-describe('FirearmControl', function() {
+describe('FirearmControl', () => {
     "use strict";
 
-    const getFirearmControl = factories.firearmControlTreeFactory;
-
     it("can calculate single fire skill checks", () => {
-        const firearm = getFirearmControl();
+        const firearm = factories.firearmControlTreeFactory();
         expect(firearm.skillCheck()).toEqual(11);
     });
 
     it("can calculate effect of missing specialization skill checks", () => {
-        const firearm = getFirearmControl({
+        const firearm = factories.firearmControlTreeFactory({
             handlerProps: {
                 skills: [{
                     skill: "Handguns",
@@ -30,8 +28,8 @@ describe('FirearmControl', function() {
         expect(firearm.skillCheck()).toEqual(40);
     });
 
-    it("notices specializations", function () {
-        var firearm = getFirearmControl({
+    it("notices specializations", () => {
+        const firearm = factories.firearmControlTreeFactory({
             handlerProps: {
                 skills: [{
                     skill: "Pistol",
@@ -46,8 +44,8 @@ describe('FirearmControl', function() {
         expect(firearm.skillCheck()).toEqual(50);
     });
 
-    it ("calculates correct ROF", function() {
-        var firearm = getFirearmControl({
+    it ("calculates correct ROF", () => {
+        const firearm = factories.firearmControlTreeFactory({
             handlerProps: {
                 skills: [{
                     skill: "Pistol",
@@ -58,8 +56,8 @@ describe('FirearmControl', function() {
         expect(firearm.rof()).toBeCloseTo(2.86, 2);
     });
 
-    it ("calculates correct ROF for higher skill level", function() {
-        var firearm = getFirearmControl({
+    it ("calculates correct ROF for higher skill level", () => {
+        const firearm = factories.firearmControlTreeFactory({
             handlerProps: {
                 skills: [{
                     skill: "Pistol",
@@ -70,8 +68,8 @@ describe('FirearmControl', function() {
         expect(firearm.rof()).toBeCloseTo(3.72, 2);
     });
 
-    it ("can calculate a row of checks", function() {
-        var firearm = getFirearmControl({
+    it ("can calculate a row of checks", () => {
+        const firearm = factories.firearmControlTreeFactory({
             handlerProps: {
                 skills: [{
                     skill: "Pistol",
@@ -83,8 +81,8 @@ describe('FirearmControl', function() {
             .toEqual([55, 48, 45, 39, 32, 25, null, null, null, null]);
     });
 
-    it ("takes into account penalties countered", function() {
-        var firearm = getFirearmControl({
+    it ("takes into account penalties countered", () => {
+        const firearm = factories.firearmControlTreeFactory({
             handlerProps: {
                 skills: [{
                     skill: "Pistol",
@@ -98,8 +96,8 @@ describe('FirearmControl', function() {
             .toEqual([55, 48, 45, 45, 38, 31, null, null, null, null]);
     });
 
-    it ("can calculate a row of initiatives", function() {
-        var firearm = getFirearmControl({
+    it ("can calculate a row of initiatives", () => {
+        const firearm = factories.firearmControlTreeFactory({
             handlerProps: {
                 character: {cur_int: 45, cur_ref: 45, cur_fit: 45,
                     cur_psy: 45},
@@ -114,8 +112,8 @@ describe('FirearmControl', function() {
     });
 
 
-    it ("can render damage", function() {
-        var firearm = getFirearmControl({
+    it ("can render damage", () => {
+        const firearm = factories.firearmControlTreeFactory({
             weapon: factories.firearmFactory({ammo: {num_dice: 2,
                 dice: 6,
                 leth: 6,
@@ -126,8 +124,8 @@ describe('FirearmControl', function() {
             .textContent).toEqual("2d6+3/6 (-1)");
     });
 
-    var getBurstController = function (givenProps) {
-        var props = {
+    const getBurstController = function (givenProps) {
+        let props = {
             fit: 45,
             hasAutofireSkill: true,
             autofireRPM: 600
@@ -135,7 +133,7 @@ describe('FirearmControl', function() {
         if (givenProps) {
             props = Object.assign(props, givenProps);
         }
-        var skills = [
+        const skills = [
             {
                 skill: "Long guns",
                 level: 0
@@ -147,7 +145,7 @@ describe('FirearmControl', function() {
                         skill: "Autofire",
                         level: 0});
         }
-        return getFirearmControl({
+        return factories.firearmControlTreeFactory({
             handlerProps: {
                 skills: skills,
                 character: {cur_ref: 45, cur_int: 45, cur_psy: 45,
@@ -164,91 +162,90 @@ describe('FirearmControl', function() {
         })});
     };
 
-    it ("can calculate burst fire checks", function () {
-        var firearm = getBurstController();
-        var checks = firearm.burstChecks([2]);
+    it ("can calculate burst fire checks", () => {
+        const firearm = getBurstController();
+        const checks = firearm.burstChecks([2]);
         expect(checks.length).toEqual(1);
         expect(checks[0].length).toEqual(5);
         expect(checks[0]).toEqual([39, 37, 33, 27, 19]);
     });
 
-    it ("can calculate burst fire checks for high FIT", function () {
-        var firearm =  getBurstController({fit: 66});
-        var checks = firearm.burstChecks([2]);
+    it ("can calculate burst fire checks for high FIT", () => {
+        const firearm =  getBurstController({fit: 66});
+        const checks = firearm.burstChecks([2]);
         expect(checks.length).toEqual(1);
         expect(checks[0].length).toEqual(5);
         expect(checks[0]).toEqual([45, 44, 40, 34, 26]);
     });
 
-    it ("does not negate bonus from low action count in bursts", function () {
-        var firearm =  getBurstController({fit: 66});
-        var checks = firearm.burstChecks([1]);
+    it ("does not negate bonus from low action count in bursts", () => {
+        const firearm =  getBurstController({fit: 66});
+        const checks = firearm.burstChecks([1]);
         expect(checks.length).toEqual(1);
         expect(checks[0].length).toEqual(5);
         expect(checks[0]).toEqual([48, 48, 48, 43, 35]);
     });
 
-    it ("takes missing Autofire skill into account", function () {
-        var firearm =  getBurstController({fit: 66, hasAutofireSkill: false});
-        var checks = firearm.burstChecks([1]);
+    it ("takes missing Autofire skill into account", () => {
+        const firearm =  getBurstController({fit: 66, hasAutofireSkill: false});
+        const checks = firearm.burstChecks([1]);
         expect(checks.length).toEqual(1);
         expect(checks[0].length).toEqual(5);
         expect(checks[0]).toEqual([38, 38, 38, 33, 25]);
     });
 
-    it ("takes low RPM into account", function () {
-        var firearm = getBurstController({autofireRPM: 400});
-        var checks = firearm.burstChecks([2]);
+    it ("takes low RPM into account", () => {
+        const firearm = getBurstController({autofireRPM: 400});
+        const checks = firearm.burstChecks([2]);
         expect(checks.length).toEqual(1);
         expect(checks[0].length).toEqual(5);
         expect(checks[0]).toEqual([39, 37, 33, null, null]);
     });
 
-    it ("takes restricted bursts into account", function () {
-        var firearm = getBurstController({restrictedBurstRounds: 3});
-        var checks = firearm.burstChecks([2]);
+    it ("takes restricted bursts into account", () => {
+        const firearm = getBurstController({restrictedBurstRounds: 3});
+        const checks = firearm.burstChecks([2]);
         expect(checks.length).toEqual(1);
         expect(checks[0].length).toEqual(5);
         expect(checks[0]).toEqual([39, 37, 33, null, null]);
     });
 
-    it ("can render sweep fire", function () {
-        var firearm = getBurstController();
-        var checks = firearm.sweepChecks(20);
+    it ("can render sweep fire", () => {
+        const firearm = getBurstController();
+        const checks = firearm.sweepChecks(20);
         expect(checks.length).toEqual(16);
         expect(checks).toEqual([55, 53, 51, 49, 45, 41, 37, 33, 23, 13, 3,
             -7, -27, -47, -67, -87]);
     });
 
-    it ("does not render sweep fire if it is disabled", function () {
-        var firearm = getBurstController({sweepFireDisabled: true});
-        var table = firearm.renderSweepTable();
+    it ("does not render sweep fire if it is disabled", () => {
+        const firearm = getBurstController({sweepFireDisabled: true});
+        const table = firearm.renderSweepTable();
         expect(table).toEqual('');
     });
 
-    it ("takes missing Autofire skill into account in sweep fire", function () {
-        var firearm = getBurstController({hasAutofireSkill: false});
-        var checks = firearm.sweepChecks(20);
+    it ("takes missing Autofire skill into account in sweep fire", () => {
+        const firearm = getBurstController({hasAutofireSkill: false});
+        const checks = firearm.sweepChecks(20);
         expect(checks.length).toEqual(16);
         expect(checks).toEqual([45, 43, 41, 39, 35, 31, 27, 23, 13, 3, -7,
             -17, -37, -57, -77, -97]);
     });
 
-    it ("counters sweep fire penalties with high FIT", function () {
-        var firearm = getBurstController({fit: 72});
-        var checks = firearm.sweepChecks(10);
+    it ("counters sweep fire penalties with high FIT", () => {
+        const firearm = getBurstController({fit: 72});
+        const checks = firearm.sweepChecks(10);
         expect(checks.length).toEqual(8);
         expect(checks).toEqual([45, 45, 45, 44, 34, 24, 4, -16]);
     });
 
-    it ("allows removal of the firearm", function () {
-        var spy = jasmine.createSpy("callback");
-        var firearm = getFirearmControl({
+    it ("allows removal of the firearm", () => {
+        const spy = jasmine.createSpy("callback");
+        const firearm = factories.firearmControlTreeFactory({
             weapon: factories.firearmFactory({id: 5}),
             onRemove: spy
         });
         TestUtils.Simulate.click(ReactDOM.findDOMNode(firearm._removeButton));
         expect(spy).toHaveBeenCalledWith({id: 5});
     });
-
 });
