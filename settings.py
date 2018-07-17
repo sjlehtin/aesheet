423a131b13db7efe64154872f89fa0754a8c3eff
Sami J. Lehtinen <sjl@iki.fi>
2018-07-17 11:39:25 +0300

    Correct DB hostname.

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ if 'RDS_HOSTNAME' in os.environ:
     DB_NAME = os.environ['RDS_DB_NAME']
     USER = os.environ['RDS_USERNAME']
     PASSWORD = os.environ['RDS_PASSWORD']
-    HOST = os.environ['RDS_HOSTNAME']
+    DBHOST = os.environ['RDS_HOSTNAME']
     PORT = os.environ['RDS_PORT']
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 elif PRODUCTION:
