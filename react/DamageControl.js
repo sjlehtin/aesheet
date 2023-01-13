7bb71dbb0cc814c3ab414ea75566a2651024c534
Sami J. Lehtinen <sjl@iki.fi>
2023-01-13 02:41:18 +0200

    Remove unnecessary componentWillReceiveProps

--- react/DamageControl.js
+++ react/DamageControl.js
@@ -52,11 +52,6 @@ class DamageControl extends React.Component {
         };
     }
 
-    componentWillReceiveProps(props) {
-        this.setState({currentStamina: props.handler.getBaseStats().stamina -
-            this.props.character.stamina_damage});
-    }
-
     handleSubmit(event) {
         var newValue = this.props.handler.getBaseStats().stamina -
             this.state.currentStamina;
