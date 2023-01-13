f01680544effb335c5ab1a7d15a9a980005a7988
Sami J. Lehtinen <sjl@iki.fi>
2023-01-13 22:31:57 +0200

    Fix to work with current webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,4 @@
 var path = require('path');
-var webpack = require('webpack');
-
-// var plugins = [
-//         new webpack.ProvidePlugin({
-//             'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
-//         })
-//     ];
-
-// const minimize = process.argv.indexOf('--no-minimize') === -1;
-//
-// if (minimize) {
-//     plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
-// }
 
 module.exports = {
     entry: "main.js",
@@ -23,8 +10,6 @@ module.exports = {
         library: "SheetApp",
         publicPath: "/static/react/"
     },
-
-    // plugins: plugins,
 
     resolve: {
         modules: [path.resolve(__dirname, 'react'), 'node_modules'],
@@ -59,49 +44,53 @@ module.exports = {
             {
                 test: /\.less$/,
                 use: [
-                "style-loader",
-                "css-loader",
-                "less-loader"
+                    {"loader": "style-loader"},
+                    {"loader": "css-loader"},
+                    {"loader": "sass-loader"}
                 ]
             },
-            // {
-            //     test: /\.scss$/,
-            //     use: [
-            //     "style-loader",
-            //     "css-loader",
-            //     "scss-loader"
-            //     ]
-            // },
-
-             {
+            {
                 test: /\.(sass|scss)$/,
-        loader: [
-          'style-loader',
-          'css-loader',
-            'sass-loader'
-        ]
-      },
+                use: [
+                    {"loader": "style-loader"},
+                    {"loader": "css-loader"},
+                    {"loader": "sass-loader"}
+                ]
+            },
 
-            { test: /\.gif$/, loader: "url-loader?mimetype=image/png" },
+            {
+                test: /\.gif$/,
+                use: {loader: "url-loader", options: {mimetype: "image/png"}}
+            },
 
-            {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
-
-//            //{ test: /\.(otf|eot|svg|ttf|woff|woff2).*$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-//            { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
-//            //{ test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
-//
-//            { test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+|#[a-z]+)?$/, loader: 'url-loader?limit=100000&mimetype=application/octet-stream' },
-//            { test: /.(png|woff(2)?|eot|ttf|svg).*$/, loader: 'url-loader?limit=100000&mimetype=application/octet-stream' },
-//            //{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-////            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-//            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-//            //{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-//
-//            //{ test: /\.css$/, loader: 'style!css'},
-//            //{ test: /\.(otf|eot|svg|ttf|woff|woff2).*$/, loader: 'url?limit=8192' }
+            {
+                test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: "url-loader",
+                    "options": {
+                        "limit": 10000,
+                        "mimetype": "application/font-woff"
+                    }
+                }
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: "url-loader",
+                    "options": {
+                        "limit": 10000,
+                        "mimetype": "application/octet-stream"
+                    }
+                }
+            },
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"},
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: "url-loader",
+                    "options": {"limit": 10000, mimetype: "image/svg+xml"}
+                }
+            }
         ]
     }
 };
