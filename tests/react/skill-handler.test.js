a3939118e8fdf7a0f83c625c455247c77293954f
Sami J. Lehtinen <sjl@iki.fi>
2023-07-19 14:32:20 +0300

    Fix code quality issues

--- tests/react/skill-handler.test.js
+++ tests/react/skill-handler.test.js
@@ -1,31 +1,17 @@
-jest.dontMock('SkillHandler');
-jest.dontMock('sheet-util');
-jest.dontMock('./factories');
-
-import React from 'react';
-
-var factories = require('./factories');
-var util = require('sheet-util');
-
+const factories = require('./factories');
 
 describe('SkillHandler', function() {
-    "use strict";
 
     it('calculates skill level', function () {
-        var handler = factories.skillHandlerFactory({skills: [
+        const handler = factories.skillHandlerFactory({skills: [
             {skill: "Pistol", level: 1}]});
 
         expect(handler.skillLevel("Pistol")).toEqual(1);
-    });
-
-    it('calculates skill check', function () {
-        var handler = factories.skillHandlerFactory({skills: [
-            {skill: "Pistol", level: 1}]});
         expect(handler.skillCheck("Pistol")).toEqual(48);
     });
 
     it('calculates skill check for zero cost skill', function () {
-        var handler = factories.skillHandlerFactory({
+        const handler = factories.skillHandlerFactory({
             character: {cur_fit: 48},
             allSkills: [
             {name: "Endurance/run", level: 0, skill_cost_0: 0, stat: "FIT"}]});
@@ -33,26 +19,26 @@ describe('SkillHandler', function() {
     });
 
     it('calculates unskilled check', function () {
-        var handler = factories.skillHandlerFactory({
+        const handler = factories.skillHandlerFactory({
             skills: [{skill: {name: "Basic Firearms", skill_cost_1: null}, level: 0}],
             allSkills: [{
                 name: "Pistol", required_skills: ["Basic Firearms"]}]
         });
-        expect(handler.skillCheck("Pistol")).toEqual(util.roundup(43/2));
+        expect(handler.skillCheck("Pistol")).toEqual(22)
     });
 
     it('calculates extremely unskilled check', function () {
-        var handler = factories.skillHandlerFactory({
+        const handler = factories.skillHandlerFactory({
             allSkills: [{
                 name: "Pistol",
                 required_skills: ["Basic Firearms"]}
                 ]
         });
-        expect(handler.skillCheck("Pistol")).toEqual(util.roundup(43/4));
+        expect(handler.skillCheck("Pistol")).toEqual(11)
     });
     
     it("mangles skill list to respect requirements order", function () {
-        var handler = factories.skillHandlerFactory({
+        const handler = factories.skillHandlerFactory({
             skills: [
                 {skill: "Naval Gunnery", level: 1},
                 {skill: "Basic Artillery", level: 0},
@@ -64,12 +50,12 @@ describe('SkillHandler', function() {
                     required_skills: ["Basic Artillery"]}
         ]});
 
-        var newList = handler.getSkillList();
+        const newList = handler.getSkillList();
         expect(newList[0].skill).toEqual("Agriculture");
     });
 
     it("finds missing skills while mangling from all requires", function () {
-        var handler = factories.skillHandlerFactory({
+        const handler = factories.skillHandlerFactory({
             skills: [
                 {skill: "Naval Gunnery", level: 1},
                 {skill: "Basic Artillery", level: 0},
@@ -81,13 +67,13 @@ describe('SkillHandler', function() {
                     required_skills: ["Basic Artillery", "Gardening"]}
         ]});
 
-        var newList = handler.getSkillList();
+        const newList = handler.getSkillList();
         expect(newList[2].skill).toEqual("Naval Gunnery");
         expect(newList[2]._missingRequired).toEqual(["Gardening"]);
     });
 
     it("does not choke on multiple required skills", function () {
-        var handler = factories.skillHandlerFactory({
+        const handler = factories.skillHandlerFactory({
             skills: [
                 {skill: "Florism", level: 1},
                 {skill: "Aesthetism", level: 0},
@@ -102,7 +88,7 @@ describe('SkillHandler', function() {
                 {name: "Aesthetism"}
         ]});
 
-        var newList = handler.getSkillList();
+        const newList = handler.getSkillList();
 
         expect(newList[0].skill).toEqual("Aesthetism");
         expect(newList[0].indent).toEqual(0);
@@ -115,21 +101,7 @@ describe('SkillHandler', function() {
 
 
     it("calculates indent for nested required skills while mangling", function () {
-        var skillList = [
-            factories.characterSkillFactory(
-                {skill: "Florism", level: 1}),
-            factories.characterSkillFactory(
-                {skill: "Gardening", level: 0}),
-            factories.characterSkillFactory(
-                {skill: "Agriculture", level: 3})
-        ];
-        var allSkills = [factories.skillFactory({name: "Agriculture"}),
-            factories.skillFactory({name: "Gardening",
-                required_skills: ["Agriculture"]}),
-            factories.skillFactory({name: "Florism",
-                required_skills: ["Gardening"]})
-        ];
-        var handler = factories.skillHandlerFactory({
+        const handler = factories.skillHandlerFactory({
             skills: [
                 {skill: "Florism", level: 1},
                 {skill: "Gardening", level: 0},
@@ -142,7 +114,7 @@ describe('SkillHandler', function() {
                 required_skills: ["Gardening"]}
         ]});
 
-         var newList = handler.getSkillList();
+        const newList = handler.getSkillList();
         expect(newList[0].skill).toEqual("Agriculture");
         expect(newList[0].indent).toEqual(0);
         expect(newList[1].indent).toEqual(1);
