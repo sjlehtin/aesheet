0bc11b5031c4973a921d62c4cadac37dc07ca5cb
Sami J. Lehtinen <sjl@iki.fi>
2024-07-12 01:35:39 +0300

    Make development version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 
 setuptools.setup(name='aesheet',
-                 version='0.22.0a5',
+                 version='0.22.0a6',
                  python_requires=">=3.8",
                  install_requires=["Django",
                                    "django-widget-tweaks",
