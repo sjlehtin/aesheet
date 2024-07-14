35a199ac44cd87d91f53de3c8230d039cb14a9ec
Sami J. Lehtinen <sjl@iki.fi>
2024-07-14 18:29:08 +0300

    Remove Skill.required_edges

--- src/sheet/models.py
+++ src/sheet/models.py
@@ -380,7 +380,6 @@ class Skill(ExportedModel):
     required_skills = models.ManyToManyField(
         "self", symmetrical=False, blank=True
     )
-    required_edges = models.ManyToManyField(Edge, blank=True)
     required_edges2 = models.ManyToManyField(Edge2, blank=True)
 
     skill_cost_0 = models.IntegerField(blank=True, null=True)
