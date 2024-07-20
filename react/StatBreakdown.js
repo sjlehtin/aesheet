640109fa4c7036dc9a9adc4bfcf5b15413d17db1
Sami J. Lehtinen <sjl@iki.fi>
2024-07-20 16:59:24 +0300

    Refactor to a function component

--- react/StatBreakdown.js
+++ react/StatBreakdown.js
@@ -1,97 +1,98 @@
-import React from "react";
-import {OverlayTrigger, Tooltip} from "react-bootstrap"
-import PropTypes from "prop-types";
+import React, {useState, useRef} from "react";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
-class StatBreakdown extends React.Component {
-    constructor(props) {
-        super(props);
+export default function StatBreakdown({
+  value: givenValue,
+  toFixed,
+  breakdown: givenBreakdown = [],
+  style = {},
+  label = "Skill check",
+  units = "",
+}) {
+  const [showOverlay, setShowOverlay] = useState(false);
 
-        this.state = {show: false};
+  let rows = [];
+
+  let calculatedToFixed = 2;
+  let coerceMainValue = false;
+  if (toFixed !== undefined) {
+    coerceMainValue = true;
+    calculatedToFixed = toFixed;
+  }
+
+  function renderValue(value) {
+    if (coerceMainValue || !Number.isInteger(value)) {
+      value = value.toFixed(calculatedToFixed);
     }
+    return value;
+  }
 
-    render() {
-        let rows = []
+  let calculatedValue;
+  let calculatedBreakdown = [];
 
-        let toFixed = 2
-        let coerceMainValue = false
-        if (this.props.toFixed !== undefined) {
-            coerceMainValue = true
-            toFixed = this.props.toFixed
-        }
+  if (typeof givenValue === "number") {
+    calculatedValue = givenValue;
+    calculatedBreakdown = givenBreakdown ?? [];
+  } else if (givenValue) {
+    calculatedValue = givenValue.value();
+    calculatedBreakdown = givenValue.breakdown();
+  }
 
-        function renderValue(value) {
-            if (coerceMainValue || !Number.isInteger(value)) {
-                value = value.toFixed(toFixed)
-            }
-            return value;
-        }
-
-        let value
-        let breakdown = []
-
-        if (typeof(this.props.value) === "number") {
-            value = this.props.value
-            breakdown = this.props.breakdown ?? []
-        } else if (this.props.value) {
-            value = this.props.value.value()
-            breakdown = this.props.value.breakdown()
-        }
-
-        breakdown.forEach((row, index) => {
-            // TODO: logic to ValueBreakdown
-            let value = row.value
-            value = renderValue(value);
-            rows.push(<tr key={index}>
-                <td>{row.reason}</td>
-                <td>{row.operation !== '+' ? row.operation ?? '' : ''}{value}</td>
-            </tr>)
-        })
-        const containerRef = React.createRef()
-        const targetRef = React.createRef()
-        const label = this.props.label ?? "Skill check"
-        return value !== undefined ? <div ref={containerRef}
-                    onClick={() => {
-                        this.setState({show: !this.state.show})
-                    }}
-                    style={this.props.style}
-                    aria-label={label}>
-            <OverlayTrigger
-                show={this.state.show}
-                onToggle={(shouldShow) => this.setState({show: shouldShow})}
-                delay={{show: 200, hide: 400}}
-                placement={"right"}
-                container={containerRef}
-                target={targetRef}
-                overlay={(props) =>
-                    <Tooltip {...props}>
-                        <div>
-                            <table aria-label={`${label} breakdown`}>
-                                <thead>
-                                <tr>
-                                    <th colSpan={2}>{`${label} breakdown`}</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {rows}
-                                </tbody>
-                            </table>
-                        </div>
-                    </Tooltip>
-                }>
-                <div ref={targetRef}>{renderValue(value)}{this.props.units}</div>
-            </OverlayTrigger>
-        </div> : ''
-    }
+  calculatedBreakdown.forEach((row, index) => {
+    // TODO: logic to ValueBreakdown
+    let value = row.value;
+    value = renderValue(value);
+    rows.push(
+      <tr key={index}>
+        <td>{row.reason}</td>
+        <td>
+          {row.operation !== "+" ? (row.operation ?? "") : ""}
+          {value}
+        </td>
+      </tr>,
+    );
+  });
+  const containerRef = useRef();
+  const targetRef = useRef();
+  const calculatedLabel = label;
+  return calculatedValue !== undefined ? (
+    <div
+      ref={containerRef}
+      onClick={() => {
+        setShowOverlay(!showOverlay);
+      }}
+      style={style}
+      aria-label={calculatedLabel}
+    >
+      <OverlayTrigger
+        show={showOverlay}
+        onToggle={(shouldShow) => setShowOverlay(shouldShow)}
+        delay={{ show: 200, hide: 400 }}
+        placement={"right"}
+        container={containerRef}
+        target={targetRef}
+        overlay={(props) => (
+          <Tooltip {...props}>
+            <div>
+              <table aria-label={`${calculatedLabel} breakdown`}>
+                <thead>
+                  <tr>
+                    <th colSpan={2}>{`${calculatedLabel} breakdown`}</th>
+                  </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+              </table>
+            </div>
+          </Tooltip>
+        )}
+      >
+        <div ref={targetRef}>
+          {renderValue(calculatedValue)}
+          {units}
+        </div>
+      </OverlayTrigger>
+    </div>
+  ) : (
+    ""
+  );
 }
-
-
-StatBreakdown.propTypes = {
-    value: PropTypes.oneOfType([PropTypes.number, PropTypes.object]).isRequired,
-    breakdown: PropTypes.arrayOf(Object),
-    style: PropTypes.object,
-    label: PropTypes.string,
-    toFixed: PropTypes.number,
-    units: PropTypes.string,
-}
-
-export default StatBreakdown
