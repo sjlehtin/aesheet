b77456124e71658cf9ca6046075e5bce6325d881
Sami J. Lehtinen <sjl@iki.fi>
2024-07-21 12:38:12 +0300

    Extract `FloatInputControl` for general use

--- react/GravityControl.js
+++ react/GravityControl.js
@@ -1,49 +1,16 @@
-import React, { useState } from "react";
-import { FloatingLabel, Form } from "react-bootstrap";
-import PropTypes from "prop-types";
-import { isFloat } from "./sheet-util";
+import React from "react";
+import FloatInputControl from "./FloatInputControl";
 
-function GravityControl({ onChange, initialValue, extraProps }) {
-  if (isFloat(initialValue)) {
-    if (Number.isInteger(initialValue)) {
-      initialValue = initialValue.toFixed(1);
-    } else {
-      initialValue = initialValue.toString();
-    }
-  }
-  const [value, setValue] = useState(initialValue ? initialValue : "");
-  const [isValid, setIsValid] = useState(true);
-
+function GravityControl({ onChange, initialValue, ...extraProps }) {
   return (
-    <>
-      <FloatingLabel controlId="gravity-input" label="Gravity" className="mb-3">
-        <Form.Control
-          onChange={(e) => {
-            let isValid, floatValue;
-            if (e.target.value === "") {
-              isValid = true;
-              floatValue = 1.0;
-            } else {
-              isValid = isFloat(e.target.value);
-              floatValue = parseFloat(e.target.value);
-            }
-            setIsValid(isValid);
-            if (isValid) {
-              onChange(floatValue);
-            }
-            setValue(e.target.value);
-          }}
-          isValid={isValid}
-          value={value}
-          type={"text"}
-          placeholder={"1.0"}
-          {...extraProps}
-        />
-      </FloatingLabel>
-    </>
+    <FloatInputControl
+      label="Gravity"
+      placeHolder={"1.0"}
+      onChange={onChange}
+      initialValue={initialValue}
+      {...extraProps}
+    />
   );
 }
-
-GravityControl.propTypes = { onChange: PropTypes.func };
 
 export default GravityControl;
