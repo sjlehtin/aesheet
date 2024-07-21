4e670df8e15899735ba976a621eb593b61e66e6e
Sami J. Lehtinen <sjl@iki.fi>
2024-07-21 12:27:45 +0300

    Use `FloatingLabel` for styling

--- react/GravityControl.js
+++ react/GravityControl.js
@@ -1,50 +1,49 @@
-import React, {useState} from 'react'
-import {Form, Row, Col} from 'react-bootstrap'
+import React, { useState } from "react";
+import { FloatingLabel, Form } from "react-bootstrap";
 import PropTypes from "prop-types";
-import {isFloat} from "./sheet-util";
+import { isFloat } from "./sheet-util";
 
-function GravityControl({onChange, initialValue}) {
-    if (isFloat(initialValue)) {
-        if (Number.isInteger(initialValue)) {
-            initialValue = initialValue.toFixed(1)
-        } else {
-            initialValue = initialValue.toString()
-        }
+function GravityControl({ onChange, initialValue, extraProps }) {
+  if (isFloat(initialValue)) {
+    if (Number.isInteger(initialValue)) {
+      initialValue = initialValue.toFixed(1);
+    } else {
+      initialValue = initialValue.toString();
     }
-    const [value, setValue] = useState(initialValue ? initialValue : "")
-    const [isValid, setIsValid] = useState(true)
+  }
+  const [value, setValue] = useState(initialValue ? initialValue : "");
+  const [isValid, setIsValid] = useState(true);
 
-    return <Row>
-        <Form.Group as={Row}>
-        <Col>
-        <Form.Label id={"gravity-input"} column sm="2">
-          Gravity
-        </Form.Label>
-        <Form.Control aria-labelledby={"gravity-input"}
-                      onChange={(e) => {
-                          let isValid, floatValue
-                          if (e.target.value === "") {
-                              isValid = true
-                              floatValue = 1.0
-                          } else {
-                              isValid = isFloat(e.target.value)
-                              floatValue = parseFloat(e.target.value)
-                          }
-                          setIsValid(isValid)
-                          if (isValid) {
-                              onChange(floatValue)
-                          }
-                          setValue(e.target.value)
-                      }}
-                      isValid={isValid}
-                      value={value}
-                      type={"text"}
-                      placeholder={"1.0"}/>
-            </Col>
-        </Form.Group>
-    </Row>;
+  return (
+    <>
+      <FloatingLabel controlId="gravity-input" label="Gravity" className="mb-3">
+        <Form.Control
+          onChange={(e) => {
+            let isValid, floatValue;
+            if (e.target.value === "") {
+              isValid = true;
+              floatValue = 1.0;
+            } else {
+              isValid = isFloat(e.target.value);
+              floatValue = parseFloat(e.target.value);
+            }
+            setIsValid(isValid);
+            if (isValid) {
+              onChange(floatValue);
+            }
+            setValue(e.target.value);
+          }}
+          isValid={isValid}
+          value={value}
+          type={"text"}
+          placeholder={"1.0"}
+          {...extraProps}
+        />
+      </FloatingLabel>
+    </>
+  );
 }
 
-GravityControl.propTypes = {onChange: PropTypes.func};
+GravityControl.propTypes = { onChange: PropTypes.func };
 
 export default GravityControl;
