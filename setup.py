120c73e0dc37193d4670a9e788ef15c10fd2f310
Sami J. Lehtinen <sjl@iki.fi>
2024-10-03 18:02:56 +0300

    Make development version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 
 setuptools.setup(name='aesheet',
-                 version='0.25.0',
+                 version='0.26.0a',
                  python_requires=">=3.8",
                  install_requires=["Django",
                                    "django-widget-tweaks",
@@ -11,7 +11,8 @@ setuptools.setup(name='aesheet',
                                    "Pillow",
                                    "psycopg2"],
                  extras_require={"dev": ["build", "pytest", "pytest-django",
-                                         "django-webtest", "factory_boy"]},
+                                         "django-webtest", "tox",
+                                         "factory_boy"]},
                  packages=setuptools.find_packages(where="src"),
                  package_dir={"": "src"},
                  package_data={"": ["templates/*/*.html", "templates/*.html"]},
