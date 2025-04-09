f362a70cb59398c713d1aa839de73f61bc694806
Sami J. Lehtinen <sjl@iki.fi>
2025-04-09 17:49:33 +0300

    Change SP addition to use modal dialog

--- react/AddSPControl.tsx
+++ react/AddSPControl.tsx
@@ -1,8 +1,8 @@
 import React, { CSSProperties } from "react";
 
-import { Button } from "react-bootstrap";
+import { Button, Form, FormControl, Modal } from "react-bootstrap";
 
-import * as util from './sheet-util';
+import * as util from "./sheet-util";
 
 function isValid(value: string) {
   return util.isInt(value);
@@ -16,6 +16,7 @@ export function AddSPControl({
   onAdd: (sp: number) => Promise<void>;
 }) {
   const [ageSP, setAgeSP] = React.useState(initialAgeSP.toString());
+  const [showDialog, setShowDialog] = React.useState(false);
 
   let inputStyle: CSSProperties = {
     textAlign: "right",
@@ -31,33 +32,73 @@ export function AddSPControl({
   function handleSubmit() {
     if (isValid(ageSP)) {
       const promise = onAdd(parseInt(ageSP));
-      setAgeSP(initialAgeSP.toString())
+      setAgeSP(initialAgeSP.toString());
+      setShowDialog(false);
       return promise;
     }
+  }
+
+  function cancelEdit() {
+    setShowDialog(false);
+    setAgeSP(initialAgeSP.toString());
   }
 
   return (
     <div title="Age SP is added every five adventures">
       <label>Age SP:</label>
-      <input
-        type="text"
-        onChange={(e) => setAgeSP(e.target.value)}
-        value={ageSP}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            /* Enter. */
-            return handleSubmit();
-          }
-        }}
-        style={inputStyle}
-      />
-      <Button
-        size="sm"
-        disabled={!isValid(ageSP)}
-        onClick={() => handleSubmit()}
-      >
+      <span style={{ paddingRight: "5px" }}>{initialAgeSP}</span>
+      <Button size="sm" onClick={() => setShowDialog(true)}>
         Add SP
       </Button>
+
+      <Modal
+        title={"Add SP"}
+        show={showDialog}
+        onHide={() => {
+          cancelEdit();
+        }}
+        onKeyDown={(e: KeyboardEvent) => {
+          if (e.key === "Escape") {
+            e.preventDefault();
+            cancelEdit();
+          }
+        }}
+      >
+        <Modal.Header closeButton={true}>
+          <Modal.Title>Add XP</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Label id={"add-sp-input"}>Add SP</Form.Label>
+
+          <FormControl
+            type="text"
+            name={"add-sp-input"}
+            aria-labelledby={"add-sp-input"}
+            autoFocus
+            onChange={(e) => setAgeSP(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                /* Enter. */
+                return handleSubmit();
+              }
+            }}
+            isValid={isValid(ageSP)}
+            className="col-xs-2"
+            value={ageSP}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={() => {
+              return handleSubmit();
+            }}
+            disabled={!isValid(ageSP)}
+            variant="primary"
+          >
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
