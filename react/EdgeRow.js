198b7a029fc528cd722f8a1ea0e8616eafbfd22e
Sami J. Lehtinen <sjl@iki.fi>
2025-04-11 21:13:52 +0300

    Refactor EdgeRow as a function component

--- react/EdgeRow.js
+++ react/EdgeRow.js
@@ -1,69 +1,52 @@
 import React from "react";
 
-import PropTypes from "prop-types";
-
 import { Button, FormCheck } from "react-bootstrap";
 
-class EdgeRow extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export function EdgeRow({ edge, onRemove, onChange, style }) {
+  let restPayload = { id: edge.id };
 
-  render() {
-    let restPayload = { id: this.props.edge.id };
-
-    return (
-      <tr
-        style={this.props.style}
-        title={this.props.edge.edge.edge.description}
-      >
-        <td>
-          {this.props.edge.edge.edge.name} {this.props.edge.edge.level}
-        </td>
-        <td>
-          <span>
-            {this.props.edge.edge.cost}
-            {this.props.edge.ignore_cost === true ? "*" : ""}
-          </span>
-        </td>
-        <td>
-          <span>
-            <FormCheck
-              aria-label={"Ignore cost"}
-              tabIndex={0}
-              onChange={(e) => {
-                this.props.onChange(
-                  Object.assign(restPayload, {
-                    ignore_cost: !this.props.edge.ignore_cost,
-                  }),
-                );
-              }}
-              checked={this.props.edge.ignore_cost}
-              type={"checkbox"}
-              value={this.props.edge.ignore_cost}
-            />
-          </span>
-        </td>
-        <td>
-          <Button
-            style={{ float: "right", paddingRight: 5 }}
-            size={"sm"}
-            onClick={(e) => {
-              this.props.onRemove(restPayload);
+  return (
+    <tr style={style} title={edge.edge.edge.description}>
+      <td>
+        {edge.edge.edge.name} {edge.edge.level}
+      </td>
+      <td>
+        <span>
+          {edge.edge.cost}
+          {edge.ignore_cost === true ? "*" : ""}
+        </span>
+      </td>
+      <td>
+        <span>
+          <FormCheck
+            aria-label={"Ignore cost"}
+            tabIndex={0}
+            onChange={(e) => {
+              onChange(
+                Object.assign(restPayload, {
+                  ignore_cost: !edge.ignore_cost,
+                }),
+              );
             }}
-          >
-            Remove
-          </Button>
-        </td>
-      </tr>
-    );
-  }
+            checked={edge.ignore_cost}
+            type={"checkbox"}
+            value={edge.ignore_cost}
+          />
+        </span>
+      </td>
+      <td>
+        <Button
+          style={{ float: "right", paddingRight: 5 }}
+          size={"sm"}
+          onClick={(e) => {
+            onRemove(restPayload);
+          }}
+        >
+          Remove
+        </Button>
+      </td>
+    </tr>
+  );
 }
-
-EdgeRow.propTypes = {
-  edge: PropTypes.object.isRequired,
-  onRemove: PropTypes.func,
-  onChange: PropTypes.func,
-};
 
 export default EdgeRow;
