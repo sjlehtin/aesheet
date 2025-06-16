b7749b57823ed45abd254fd8410f3adb8f8a2a23
Sami J. Lehtinen <sjl@iki.fi>
2025-06-17 00:24:13 +0300

    Make release 0.29.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 
 setuptools.setup(name='aesheet',
-                 version='0.28.0',
+                 version='0.29.0',
                  python_requires=">=3.8",
                  install_requires=["Django",
                                    "django-widget-tweaks",
