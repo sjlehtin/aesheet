181790c2c0c41595b8371f91ce781619186de6d7
Sami J. Lehtinen <sjl@iki.fi>
2025-06-30 23:49:31 +0300

    Allow updates without version bump

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ if [ -z "$sourcedir" ]; then
 fi
 
 sudo cp -a "$sourcedir"/static .
-sudo ve/bin/pip install -U "$sourcedir"/aesheet-*-py3-none-any.whl
+sudo ve/bin/pip install --force-reinstall -U "$sourcedir"/aesheet-*-py3-none-any.whl
 sudo -u www-data bash -c "source ./env && django-admin migrate"
 [ -x ./restart.sh ] && sudo ./restart.sh
